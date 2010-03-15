620f90f426f3dabd83558f67c79c8cc4c3032d58
Koushik K. Dutta <koush@Koushik-MacBook.local>
2010-03-15 11:21:57 -0700

    allow mirrors

--- manifest.js
+++ manifest.js
@@ -1,78 +1,108 @@
 {
-  roms: 
-  [
+    roms:
+    [
     {
-      name: "CyanogenMod 5.0.4.2",
-      device: "sholes",
-      url: "http://clockworkmod.s3.amazonaws.com/motorola/sholes/cyanogen_sholes-ota-eng.koush_5.0.4.2.zip",
-      incremental: 5042,
-      addons:
-      [
+        name: "CyanogenMod 5.0.4.2",
+        device: "sholes",
+        urls:
+        [
+        "http://clockworkmod.s3.amazonaws.com/motorola/sholes/cyanogen_sholes-ota-eng.koush_5.0.4.2.zip"
+        ],
+        incremental: 5042,
+        addons:
+        [
         {
-          name: "Modules",
-          url: "http://clockworkmod.s3.amazonaws.com/motorola/sholes/bekit-modules_0.7.zip"
+            name: "Modules",
+            urls:
+            [
+            "http://clockworkmod.s3.amazonaws.com/motorola/sholes/bekit-modules_0.7.zip"
+            ]
         }
-      ],
-      choices:
-      [
+        ],
+        choices:
+        [
         {
-          name: "Kernel",
-          options:
-          [
+            name: "Kernel",
+            options:
+            [
             {
-              name: "Overclocked",
-              url: ""
+                name: "Overclocked",
+                urls:
+                [
+                ""
+                ]
             },
             {
-              name: "Stable",
-              url: "http://clockworkmod.s3.amazonaws.com/motorola/sholes/cyanogen_sholes-ota-eng.koush_5.0.4.2.zip"
+                name: "Stable",
+                urls:
+                [
+                "http://clockworkmod.s3.amazonaws.com/motorola/sholes/cyanogen_sholes-ota-eng.koush_5.0.4.2.zip"
+                ]
             }
-          ]
+            ]
         }
-      ] 
+        ]
     },
     {
-      name: "CyanogenMod 5.0.4.1 (JIT)",
-      device: "sholes",
-      url: "http://clockworkmod.s3.amazonaws.com/motorola/sholes/cyanogen_sholes-ota-eng.koush_5.0.4.1.zip",
-      incremental: 5042,
-      addons:
-      [
+        name: "CyanogenMod 5.0.4.1 (JIT)",
+        device: "sholes",
+        urls:
+        [
+        "http://clockworkmod.s3.amazonaws.com/motorola/sholes/cyanogen_sholes-ota-eng.koush_5.0.4.1.zip"
+        ],
+        incremental: 5042,
+        addons:
+        [
         {
-          name: "Modules",
-          url: "http://clockworkmod.s3.amazonaws.com/motorola/sholes/bekit-modules_0.7.zip"
+            name: "Modules",
+            urls:
+            [
+            "http://clockworkmod.s3.amazonaws.com/motorola/sholes/bekit-modules_0.7.zip"
+            ]
         }
-      ],
-      choices:
-      [
+        ],
+        choices:
+        [
         {
-          name: "Kernel",
-          options:
-          [
+            name: "Kernel",
+            options:
+            [
             {
-              name: "Overclocked",
-              url: ""
+                name: "Overclocked",
+                urls:
+                [
+                ""
+                ]
             },
             {
-              name: "Stable",
-              url: "http://clockworkmod.s3.amazonaws.com/motorola/sholes/cyanogen_sholes-ota-eng.koush_5.0.4.2.zip"
+                name: "Stable",
+                urls:
+                [
+                "http://clockworkmod.s3.amazonaws.com/motorola/sholes/cyanogen_sholes-ota-eng.koush_5.0.4.2.zip"
+                ]
             }
-          ]
+            ]
         }
-      ] 
+        ]
     },
     {
-      name: "CyanogenMod 5.0.4.1",
-      device: "passion",
-      url: "http://n0rp.chemlab.org/android/nexus/update-cm-5.0.4.1-N1-signed.zip",
-      incremental: 5041,
-      addons:
-      [
+        name: "CyanogenMod 5.0.4.1",
+        device: "passion",
+        urls:
+        [
+        "http://n0rp.chemlab.org/android/nexus/update-cm-5.0.4.1-N1-signed.zip"
+        ],
+        incremental: 5041,
+        addons:
+        [
         {
-          name: "Google Apps",
-          url: "http://clockworkmod.s3.amazonaws.com/common/gapps-passion-ERE36B-2-signed.zip"
+            name: "Google Apps",
+            urls:
+            [
+            "http://clockworkmod.s3.amazonaws.com/common/gapps-passion-ERE36B-2-signed.zip"
+            ]
         }
-      ]
+        ]
     }
-  ]
+    ]
 }

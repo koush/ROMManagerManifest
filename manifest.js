85f9de2cf4571540f13a706a39e7e67792aeecbc
Koushik K. Dutta <koush@Koushik-OSx86.local>
2010-03-15 01:39:12 -0700

    google addon for n1

--- manifest.js
+++ manifest.js
@@ -36,7 +36,14 @@
       name: "CyanogenMod 5.0.4.1",
       device: "passion",
       url: "http://n0rp.chemlab.org/android/nexus/update-cm-5.0.4.1-N1-signed.zip",
-      incremental: 5041
+      incremental: 5041,
+      addons:
+      [
+        {
+          name: "Google Apps",
+          url: "http://clockworkmod.s3.amazonaws.com/common/gapps-passion-ERE36B-2-signed.zip"
+        }
+      ]
     }
   ]
 }

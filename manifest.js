03c61015fe569613693393613c8f8f8dc5da24f8
Koushik K. Dutta <koush@Koushik-MacBook.local>
2010-03-15 13:35:38 -0700

    5.0.4.23

--- manifest.js
+++ manifest.js
@@ -2,6 +2,55 @@
     roms:
     [
     {
+        name: "CyanogenMod 5.0.4.3",
+        device: "passion",
+        urls:
+        [
+        "http://clockworkmod.s3.amazonaws.com/motorola/sholes/cyanogen_sholes-ota-eng.koush_5.0.4.3.zip"
+        ],
+        incremental: 5041,
+        addons:
+        [
+        {
+            name: "Google Apps",
+            urls:
+            [
+            "http://clockworkmod.s3.amazonaws.com/common/gapps-passion-ERE36B-2-signed.zip"
+            ]
+        },
+        {
+            name: "Modules",
+            urls:
+            [
+            "http://clockworkmod.s3.amazonaws.com/motorola/sholes/bekit-modules_0.7.zip"
+            ]
+        }
+        ],
+        choices:
+        [
+        {
+            name: "Kernel",
+            options:
+            [
+            {
+                name: "Overclocked",
+                urls:
+                [
+                ""
+                ]
+            },
+            {
+                name: "Stable",
+                urls:
+                [
+                "http://clockworkmod.s3.amazonaws.com/motorola/sholes/cyanogen_sholes-ota-eng.koush_5.0.4.2.zip"
+                ]
+            }
+            ]
+        }
+        ]
+    },
+    {
         name: "CyanogenMod 5.0.4.2",
         device: "sholes",
         urls:

79432feab372a1942d17c2d461a198ae91deda4c
Koushik K. Dutta <koush@Koushik-MacBook.local>
2010-03-22 01:05:41 -0700

    add sample.js

---
+++ sample.js
@@ -0,0 +1,14 @@
+{
+    // This is what name you want your ROMs to show up under.
+    developer: "CyanogenMod",
+    // The display summary. Two lines max.
+    summary: "Mods and bacon for Dream, Sapphire, Droid, and Nexus One",
+    // The location of the developer's manifest.
+    manifest: "http://github.com/koush/ROMManagerManifest/raw/master/manifest2.js",
+    // The list of devices that this developer's ROMs support.
+    roms:
+    {
+        sholes: true,
+        passion: true
+    },
+}

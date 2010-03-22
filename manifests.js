70df0b850ae86c628986a5ab6cc86e0e544f2b06
Koushik K. Dutta <koush@Koushik-MacBook.local>
2010-03-22 00:05:41 -0700

    fix parse error

--- manifests.js
+++ manifests.js
@@ -3,7 +3,7 @@
   [
     {
       developer: "CyanogenMod",
-      summary: "Mods and bacon for Dream, Sapphire, Droid, and Nexus One"
+      summary: "Mods and bacon for Dream, Sapphire, Droid, and Nexus One",
       free: true,
       manifest: "http://github.com/koush/ROMManagerManifest/raw/master/manifest2.js",
       roms:

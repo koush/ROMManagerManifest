d6ffb4a127cac475643d1e3e58a2c756e6a57553
Koushik K. Dutta <koush@Koushik-MacBook.local>
2010-03-22 01:03:23 -0700

    comments!

--- manifest2.js
+++ manifest2.js
@@ -1,19 +1,31 @@
 {
-    // TODO: remove this
-    minversion: 180,
     roms:
     [
     {
+        // Display name of the rom
         name: "CyanogenMod 5.0.5",
+        // Display summary of the rom
         summary: "Stable",
+        // Device that this rom runs on
         device: "sholes",
+        // Product name of the mod. Product names should stay the same between versions of the mod.
+        product: "CyanogenMod",
+        // Numerical version of the mod. This allows the OTA system to figure out
+        // when an upgrade is available. So, if the current version is 5.0.5.0,
+        // you can set "incremental" to 5050. And when you have an upgrade available,
+        // set the "incremental" of that ROM to 5051 (or any number higher than 5050).
+        // ROM Manager will then figure out that an upgrade is available and let
+        // the user know.
+        incremental: 5050,
+        // The value of ro.modversion in /system/build.prop. Your ROM should set this
+        // in that build.prop so that ROM Manager can figure out what ROM is currently running.
+        modversion: "CyanogenMod-5.0.5-Droid",
         urls:
         [
         "http://www.droidaftermarket.com/koush/motorola/sholes/cyanogen_sholes-ota-eng.koush_5.0.5.zip",
         "http://koush.romraid.com/motorola/sholes/cyanogen_sholes-ota-eng.koush_5.0.5.zip",
         "http://alldroid.org/roms/motorola/droid/koush/motorola/sholes/cyanogen_sholes-ota-eng.koush_5.0.5.zip",
         ],
-        incremental: 5050,
         addons:
         [
         {
@@ -31,62 +43,6 @@
             "http://koush.romraid.com/motorola/sholes/bekit-modules.zip",
             "http://alldroid.org/roms/motorola/droid/koush//motorola/sholes/bekit-modules.zip",
             "http://www.droidaftermarket.com/koush//motorola/sholes/bekit-modules.zip",
-            ]
-        }
-        ]
-    },
-    {
-        name: "CyanogenMod 5.0.4.3",
-        summary: "Experimental",
-        device: "sholes",
-        urls:
-        [
-        "http://www.droidaftermarket.com/koush/motorola/sholes/cyanogen_sholes-ota-eng.koush_5.0.4.3.zip",
-        "http://koush.romraid.com/motorola/sholes/cyanogen_sholes-ota-eng.koush_5.0.4.3.zip",
-        "http://alldroid.org/roms/motorola/droid/koush/motorola/sholes/cyanogen_sholes-ota-eng.koush_5.0.4.3.zip",
-        ],
-        incremental: 5043,
-        addons:
-        [
-        {
-            name: "Google Apps",
-            urls:
-            [
-            "http://koush.romraid.com/common/gapps-passion-ERE36B-2-signed.zip",
-            "http://alldroid.org/roms/motorola/droid/koush//common/gapps-passion-ERE36B-2-signed.zip",
-            "http://www.droidaftermarket.com/koush//common/gapps-passion-ERE36B-2-signed.zip",
-            ]
-        },
-        {
-            name: "Modules",
-            urls:
-            [
-            "http://koush.romraid.com/motorola/sholes/bekit-modules.zip",
-            "http://alldroid.org/roms/motorola/droid/koush//motorola/sholes/bekit-modules.zip",
-            "http://www.droidaftermarket.com/koush//motorola/sholes/bekit-modules.zip",
-            ]
-        }
-        ],
-        choices:
-        [
-        {
-            name: "Kernel",
-            options:
-            [
-            {
-                name: "Overclocked",
-                urls:
-                [
-                ""
-                ]
-            },
-            {
-                name: "Stable",
-                urls:
-                [
-                "http://clockworkmod.s3.amazonaws.com/motorola/sholes/kernel_nooverlock_1.0.2.zip"
-                ]
-            }
             ]
         }
         ]
@@ -95,13 +51,15 @@
         name: "CyanogenMod 5.0.4.2",
         summary: "Stable",
         device: "sholes",
+        product: "CyanogenMod",
+        incremental: 5042,
+        modversion: "CyanogenMod-5.0.4.2-Droid",
         urls:
         [
         "http://koush.romraid.com/motorola/sholes/cyanogen_sholes-ota-eng.koush_5.0.4.2.zip",
         "http://alldroid.org/roms/motorola/droid/koush/motorola/sholes/cyanogen_sholes-ota-eng.koush_5.0.4.2.zip",
         "http://www.droidaftermarket.com/koush/motorola/sholes/cyanogen_sholes-ota-eng.koush_5.0.4.2.zip",
         ],
-        incremental: 5042,
         addons:
         [
         {
@@ -138,63 +96,18 @@
         }
         ]
     },
-    {
-        name: "CyanogenMod 5.0.4.1",
-        summary: "Experimental (JIT)",
-        device: "sholes",
-        urls:
-        [
-        "http://koush.romraid.com/motorola/sholes/cyanogen_sholes-ota-eng.koush_5.0.4.1.zip",
-        "http://alldroid.org/roms/motorola/droid/koush/motorola/sholes/cyanogen_sholes-ota-eng.koush_5.0.4.1.zip",
-        "http://koushikdutta.blurryfox.com/motorola/sholes/cyanogen_sholes-ota-eng.koush_5.0.4.1.zip",
-        "http://www.droidaftermarket.com/koush/motorola/sholes/cyanogen_sholes-ota-eng.koush_5.0.4.2.zip",
-        ],
-        incremental: 5041,
-        addons:
-        [
-        {
-            name: "Modules",
-            urls:
-            [
-            "http://koush.romraid.com/motorola/sholes/bekit-modules.zip",
-            "http://alldroid.org/roms/motorola/droid/koush//motorola/sholes/bekit-modules.zip",
-            "http://www.droidaftermarket.com/koush//motorola/sholes/bekit-modules.zip",
-            ]
-        }
-        ],
-        choices:
-        [
-        {
-            name: "Kernel",
-            options:
-            [
-            {
-                name: "Overclocked",
-                urls:
-                [
-                ""
-                ]
-            },
-            {
-                name: "Stable",
-                urls:
-                [
-                "http://clockworkmod.s3.amazonaws.com/motorola/sholes/kernel_nooverlock_1.0.2.zip"
-                ]
-            }
-            ]
-        }
-        ]
-    },
+
     {
         name: "CyanogenMod 5.0.5",
         summary: "Stable",
         device: "passion",
+        product: "CyanogenMod",
+        incremental: 5050,
+        modversion: "CyanogenMod-5.0.5-N1",
         urls:
         [
         "http://cyanogen-updater.googlecode.com/files/update-cm-5.0.5-N1-signed.zip"
         ],
-        incremental: 5050,
         addons:
         [
         {
@@ -211,11 +124,13 @@
         name: "CyanogenMod 5.0.4.1",
         summary: "Stable",
         device: "passion",
+        product: "CyanogenMod",
+        incremental: 5041,
+        modversion: "CyanogenMod-5.0.4.1-N1",
         urls:
         [
         "http://cyanogen-updater.googlecode.com/files/update-cm-5.0.4.1-N1-signed.zip"
         ],
-        incremental: 5041,
         addons:
         [
         {
@@ -229,5 +144,7 @@
         }
         ]
     }
-    ]
+    ],
+    // TODO: remove this
+    minversion: 180,
 }
--- manifests.js
+++ manifests.js
@@ -3,10 +3,15 @@
     manifests:
     [
     {
+        // This is what name you want your ROMs to show up under.
         developer: "CyanogenMod",
+        // The display summary. Two lines max.
         summary: "Mods and bacon for Dream, Sapphire, Droid, and Nexus One",
+        // Is this a free ROM? This is decided by Koush/Cyanogen/CTSO.
         free: true,
+        // The location of the developer's manifest.
         manifest: "http://github.com/koush/ROMManagerManifest/raw/master/manifest2.js",
+        // The list of devices that this developer's ROMs support.
         roms:
         {
             sholes: true,

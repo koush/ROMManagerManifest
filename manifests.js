00cc2478249c7a21a0458783a377a450c1449136
Koushik K. Dutta <koush@Koushik-MacBook.local>
2010-03-22 00:04:10 -0700

    initial commit of manifests.js

---
+++ manifests.js
@@ -0,0 +1,26 @@
+{
+  manifests:
+  [
+    {
+      developer: "CyanogenMod",
+      summary: "Mods and bacon for Dream, Sapphire, Droid, and Nexus One"
+      free: true,
+      manifest: "http://github.com/koush/ROMManagerManifest/raw/master/manifest2.js",
+      roms:
+      {
+        sholes: true,
+        passion: true
+      },
+    },
+    {
+      developer: "HoboDeveloper (Premium ROM)",
+      summary: "King of the Copy and Paste ROMs"
+      manifest: "http://github.com/koush/ROMManagerManifest/raw/master/manifest2.js",
+      roms:
+      {
+        sholes: true,
+        passion: true
+      },
+    }
+  ]
+}

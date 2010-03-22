5ef43e1fcce19274f1e2baa756fef82aa8005077
Koushik K. Dutta <koush@Koushik-MacBook.local>
2010-03-22 00:27:56 -0700

    remove sholes

--- manifests.js
+++ manifests.js
@@ -1,26 +1,26 @@
 {
-  manifests:
-  [
+    manifests:
+    [
     {
-      developer: "CyanogenMod",
-      summary: "Mods and bacon for Dream, Sapphire, Droid, and Nexus One",
-      free: true,
-      manifest: "http://github.com/koush/ROMManagerManifest/raw/master/manifest2.js",
-      roms:
-      {
-        sholes: true,
-        passion: true
-      },
+        developer: "CyanogenMod",
+        summary: "Mods and bacon for Dream, Sapphire, Droid, and Nexus One",
+        free: true,
+        manifest: "http://github.com/koush/ROMManagerManifest/raw/master/manifest2.js",
+        roms:
+        {
+            sholes: true,
+            passion: true
+        },
     },
     {
-      developer: "HoboDeveloper (Premium)",
-      summary: "King of the Copy and Paste ROMs. This ROM is intentionally broken for testing purposes.",
-      manifest: "http://github.com/koush/ROMManagerManifest/raw/master/manifest.js",
-      roms:
-      {
-        sholes: true,
-        passion: true
-      },
+        developer: "HoboDeveloper (Premium)",
+        summary: "King of the Copy and Paste ROMs. This ROM is intentionally broken for testing purposes.",
+        manifest: "http://github.com/koush/ROMManagerManifest/raw/master/manifest.js",
+        roms:
+        {
+            sholes: true,
+            passion: true
+        },
     }
-  ]
+    ]
 }

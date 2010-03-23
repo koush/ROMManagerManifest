6ffd8181af7e16c9bc8971017c93c38f46fdb694
Koushik K. Dutta <koush@Koushik-MacBook.local>
2010-03-23 14:47:27 -0700

    add devices manifest

---
+++ devices.js
@@ -0,0 +1,55 @@
+{
+    recovery_url: "http://koush.romraid.com/recoveries/recovery-clockwork-%s-%s.img",
+    version: "1.7.7.0",
+    
+    devices:
+    [
+
+    {
+        init: "init.sholes.rc",
+        key: "sholes",
+        name: "Motorola Droid (CDMA)"
+    },
+
+    {
+        init: "init.sapphire.rc",
+        key: "sapphire",
+        name: "T-Mobile MyTouch 3G (GSM)"
+    },
+
+    {
+        init: "init.sapphire.rc",
+        key: "magic",
+        name: "HTC Magic (GSM)"
+    },
+
+    {
+        init: "init.trout.rc",
+        key: "dream",
+        name: "HTC Dream (GSM)"
+    },
+
+    {
+        init: "init.mahimahi.rc",
+        key: "passion",
+        name: "Google Nexus One (GSM)"
+    },
+
+    {
+        init: "init.hero.rc",
+        key: "hero",
+        name: "HTC Hero (GSM)"
+    },
+
+    {
+        init: "init.heroc.rc",
+        key: "heroc",
+        name: "HTC Hero (CDMA)"
+    },
+
+    {
+        init: "init.desirec.rc",
+        key: "desirec",
+        name: "Droid Eris (CDMA)"
+    }
+}

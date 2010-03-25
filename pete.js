14f09e5723fd4417312af41c4a447b1b430072eb
Koushik K. Dutta <koush@Koushik-MacBook.local>
2010-03-25 00:14:54 -0700

    add chevy kernel options

--- pete.js
+++ pete.js
@@ -11,6 +11,50 @@
         urls:
         [
         "http://androidoverdrive.com/pete/ESE53/V0.9/update.zip",
+        ],
+        choices:
+        [
+        {
+            name: "Kernel",
+            options:
+            [
+            {
+                name: "Default",
+                urls:
+                [
+                ""
+                ]
+            },
+            {
+                name: "ChevyNo1 1.0Ghz OC 5 slot",
+                urls:
+                [
+                "http://koush.romraid.com/motorola/sholes/chevy/ChevyNo1-10Ghz-OC-5slot-update.zip",
+                ]
+            },
+            {
+                name: "ChevyNo1 1.1Ghz OC 5 slot",
+                urls:
+                [
+                "http://koush.romraid.com/motorola/sholes/chevy/ChevyNo1-11Ghz-OC-5slot-update.zip",
+                ]
+            },
+            {
+                name: "ChevyNo1 1.2Ghz OC 5 slot",
+                urls:
+                [
+                "http://koush.romraid.com/motorola/sholes/chevy/ChevyNo1-12Ghz-OC-5slot-update.zip",
+                ]
+            },
+            {
+                name: "ChevyNo1 1.25Ghz OC 5 slot",
+                urls:
+                [
+                "http://koush.romraid.com/motorola/sholes/chevy/ChevyNo1-125Ghz-OC-5slot-update.zip",
+                ]
+            },
+            ]
+        }
         ]
     }
     ]

0e52727401d7889be94f9d2b76ebf47b0afb65fa
Koushik K. Dutta <koush@Koushik-MacBook.local>
2010-03-25 01:29:44 -0700

    add nextheme

--- manup456.js
+++ manup456.js
@@ -3,6 +3,17 @@
     roms:
     [
     {
+        name: "NexTheme-v2.0",
+        summary: "Stable",
+        device: "passion",
+        product: "NexTheme",
+        incremental: 2000,
+        urls:
+        [
+        "http://www.androidspin.com/downloads.php?dir=manup456/THEMES/&file=NexTHEME-v2.0-Enom_1.8.1_Final.zip",
+        ]
+    },
+    {
         name: "NexDesire-v1.2 REMIX!",
         summary: "Stable",
         device: "passion",

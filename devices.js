be47e5da0cd514149a78a1634897a6dbf8387c3b
Koushik K. Dutta <koush@Koushik-MacBook.local>
2010-03-26 12:35:43 -0700

    add alternate recovery urls

--- devices.js
+++ devices.js
@@ -9,50 +9,88 @@
         init: "init.sholes.rc",
         key: "sholes",
         name: "Motorola Droid (CDMA)",
-        version: "1.7.7.1"
+        alternate_recovery:
+        {
+            name: "SPRecovery 0.99.3b",
+            url: "http://downloads.droidmod.org/recovery-0.99.3b.img"
+        }
     },
 
     {
         init: "init.sapphire.rc",
         key: "sapphire",
-        name: "T-Mobile MyTouch 3G (GSM)"
+        name: "T-Mobile MyTouch 3G (GSM)",
+        alternate_recovery:
+        {
+            name: "RA Recovery v.1.6.2",
+            url: "http://www.androidspin.com/downloads.php?dir=amon_ra/RECOVERY/&file=recovery-RA-sapphire-v1.6.2G.img"
+        }
     },
 
     {
         init: "init.sapphire.rc",
         key: "magic",
-        name: "HTC Magic (GSM)"
+        name: "HTC Magic (GSM)",
+        alternate_recovery:
+        {
+            name: "RA Recovery v.1.6.2",
+            url: "http://www.androidspin.com/downloads.php?dir=amon_ra/RECOVERY/&file=recovery-RA-sapphire-v1.6.2H.img"
+        }
     },
 
     {
         init: "init.trout.rc",
         key: "dream",
-        name: "HTC Dream (GSM)"
+        name: "HTC Dream (GSM)",
+        alternate_recovery:
+        {
+            name: "RA Recovery v.1.6.2",
+            url: "http://www.androidspin.com/downloads.php?dir=amon_ra/RECOVERY/&file=recovery-RA-dream-v1.6.2.img"
+        }
     },
 
     {
         init: "init.mahimahi.rc",
         key: "passion",
-        name: "Google Nexus One (GSM)",
-        version: "1.7.7.1"
+        name: "Google Nexus One (GSM)",,
+        alternate_recovery:
+        {
+            name: "RA Recovery v.1.7.0",
+            url: "http://www.androidspin.com/downloads.php?dir=amon_ra/RECOVERY/&file=recovery-RA-nexus-v1.7.0.img"
+        }
     },
 
     {
         init: "init.hero.rc",
         key: "hero",
-        name: "HTC Hero (GSM)"
+        name: "HTC Hero (GSM)",
+        alternate_recovery:
+        {
+            name: "RA Recovery v.1.6.2",
+            url: "http://www.androidspin.com/downloads.php?dir=amon_ra/RECOVERY/&file=recovery-RA-hero-v1.6.2.img"
+        }
     },
 
     {
         init: "init.heroc.rc",
         key: "heroc",
-        name: "HTC Hero (CDMA)"
+        name: "HTC Hero (CDMA)",
+        alternate_recovery:
+        {
+            name: "RA Recovery v.1.6.2",
+            url: "http://www.androidspin.com/downloads.php?dir=amon_ra/RECOVERY/&file=recovery-RA-heroc-v1.6.2.img"
+        }
     },
 
     {
         init: "init.desirec.rc",
         key: "desirec",
-        name: "Droid Eris (CDMA)"
+        name: "Droid Eris (CDMA)",
+        alternate_recovery:
+        {
+            name: "RA Recovery v.1.6.2",
+            url: "http://www.androidspin.com/downloads.php?dir=amon_ra/RECOVERY/&file=recovery-RA-eris-v1.6.2.img"
+        }
     }
     
     ]

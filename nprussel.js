9da233f656c4d77439d05e83eb3c10b74c02dd1b
Koushik K. Dutta <koush@Koushik-MacBook.local>
2010-03-26 07:51:49 -0700

    add google search fix

--- nprussel.js
+++ nprussel.js
@@ -7,11 +7,26 @@
         summary: "Beta2",
         device: "hero",
         product: "nprussel",
+        donate: "https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=67SD3VN55DGYG",
         incremental: 4000,
-        urls:
+        extendedurls:
         [
-        "http://www.villainrom.co.uk/villain4Beta2.zip",
-        "http://www.villainrom.co.uk/usmirror/villain4Beta2.zip",
+        {
+          name: "VillainROM4.0",
+          urls:
+          [
+          "http://www.villainrom.co.uk/villain4Beta2.zip",
+          "http://www.villainrom.co.uk/usmirror/villain4Beta2.zip",
+          ]
+        },
+        {
+          name: "Google Search Fix",
+          urls:
+          [
+          "http://koush.romraid.com//htc/hero/VillainROM4.0B2-GoogleSearchFix-signed.zip",
+          "http://www.droidaftermarket.com/koush//htc/hero/VillainROM4.0B2-GoogleSearchFix-signed.zip"
+          ]
+        }
         ]
     }
     ]

4e1ccf463b0f6b2521ac7240c88c413d73d81238
Koushik K. Dutta <koush@Koushik-MacBook.local>
2010-03-28 22:57:07 -0700

    missing file

---
+++ stock.js
@@ -0,0 +1,17 @@
+{
+    version: 1,
+    roms:
+    [
+    {
+        name: "Verizon Droid 2.0.1 Stock",
+        summary: "Stable",
+        device: "sholes",
+        product: "stock",
+        urls:
+        [
+        "http://koush.romraid.com//motorola/sholes/update-stock.zip",
+        "http://www.droidaftermarket.com/koush//motorola/sholes/update-stock.zip"
+        ]
+    }
+}
+    

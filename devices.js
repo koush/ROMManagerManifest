00dda6f32d3622b5f35e3e7af65c3017534b3836
Koushik K. Dutta <koush@Koushik-MacBook.local>
2010-03-28 23:01:54 -0700

    step sholes to 1.7.8.6

--- devices.js
+++ devices.js
@@ -9,6 +9,7 @@
         init: "init.sholes.rc",
         key: "sholes",
         name: "Motorola Droid (CDMA)",
+        version: "1.7.8.6",
         alternate_recovery:
         {
             name: "SPRecovery 0.99.3b",

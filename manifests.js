5b3b762d427b9c08f3d23e89166ada8ba42cc826
Koushik K. Dutta <koush@Koushik-MacBook.local>
2010-03-30 19:22:48 -0700

    add forkup

--- manifests.js
+++ manifests.js
@@ -91,6 +91,24 @@
         },
     },
     {
+        // This is what name you want your ROMs to show up under.
+        developer: "Forkup",
+        // Your id. This should have no spaces or other funny characters.
+        // This would ideally be your Github id, so when you send me your manifest
+        // I can verify who you are. But it can be anything really.
+        id: "forkup",
+        // The display summary. Two lines max.
+        summary: "Buuf Theme and Flavored Ice for Cyanogen 5.0.5.4 for the Droid.",
+        // The location of the developer's manifest.
+        manifest: "http://www.forkup.info/droidthemes/forkup.js",
+        // The list of devices that this developer's ROMs support.
+        // If you don't support a device, just don't list it!
+        roms:
+        {
+            sholes: true,
+        },
+    },
+    {
         developer: "Nexus-Nation",
         id: "manup456",
         summary: "Themes, Rom's and more for the Nexus One",

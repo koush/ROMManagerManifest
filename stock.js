492bcb28cae5ec522330d8a96e8f1882964bb398
Koushik K. Dutta <koush@Koushik-MacBook.local>
2010-03-31 12:26:45 -0700

    add 2.1 stock droid

--- stock.js
+++ stock.js
@@ -3,8 +3,20 @@
     roms:
     [
     {
+        name: "Verizon Droid 2.1 Stock",
+        summary: "Stable, Radio, Rooted",
+        device: "sholes",
+        product: "stock",
+        urls:
+        [
+        "http://koush.romraid.com//motorola/sholes/ESE81update.zip",
+        "http://android.d3xt3r01.tk/koush//motorola/sholes/ESE81update.zip",
+        "http://www.droidaftermarket.com/koush//motorola/sholes/ESE81update.zip"
+        ]
+    },
+    {
         name: "Verizon Droid 2.0.1 Stock",
-        summary: "Stable",
+        summary: "Stable, NOT Rooted",
         device: "sholes",
         product: "stock",
         urls:

6d32295db084ddfbd331b0e9565bf7ee3c6bae26
Koushik K. Dutta <koush@Koushik-MacBook.local>
2010-04-02 11:10:55 -0700

    rename smoked glass

--- manifests.js
+++ manifests.js
@@ -149,7 +149,7 @@
         },
     },
     {
-        developer: "Smoked Glass for CyanogenMod",
+        developer: "Smoked Glass-esque",
         id: "m3kilpat",
         summary: "Smoked glass theme that can be installed over CyanogenMod 5.0.5.4",
         manifest: "http://github.com/m3kilpat/ROMManagerManifest/raw/master/kilpat.js",

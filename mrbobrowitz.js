61c3ea7145baa38237748bb9acc454c7f12cb96e
Koushik K. Dutta <koush@Koushik-MacBook.local>
2010-04-06 22:01:46 -0700

    add clean looks

---
+++ mrbobrowitz.js
@@ -0,0 +1,16 @@
+{
+    version: 1,
+    roms:
+    [
+    {
+        name: "Clean Looks v1",
+        summary: "Stable, installed OVER CyanogenMod 5.0.5.7",
+        device: "sholes",
+        product: "CleanLooks",
+        incremental: 1000,
+        urls:
+        [
+        "http://dl.dropbox.com/u/2070814/Droid/CleanLooksRelease1.zip"
+        ]
+    }
+}

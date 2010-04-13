46b086fbd3e0308d7ce2763f1fba1b48de8047e1
Koushik K. Dutta <koush@Koushik-MacBook.local>
2010-04-13 15:27:44 -0700

    UD is no longer free

--- manifests.js
+++ manifests.js
@@ -132,7 +132,6 @@
     },
     {
         developer: "Ultimate Droid",
-        free: true,
         id: "blackdroid",
         summary: "Another CyanogenMod based theme!",
         manifest: "http://www.clockworkmod.com/ROMManagerManifest/blackdroid.js",

ed7d1834e5943bf468c2070571767f8585cb2faf
Koushik K. Dutta <koush@Koushik-MacBook.local>
2010-04-16 00:05:39 -0700

    remove 5.0.6 because I FAIL AT WIFI

--- cyanogenmod.js
+++ cyanogenmod.js
@@ -324,50 +324,6 @@
     },
     {
         // Display name of the rom
-        name: "CyanogenMod 5.0.6",
-        // Display summary of the rom
-        summary: "Stable",
-        // Device that this rom runs on
-        device: "sholes",
-        // Product name of the mod. Product names should stay the same between versions of the mod.
-        product: "CyanogenMod",
-        // Numerical version of the mod. This allows the OTA system to figure out
-        // when an upgrade is available. So, if the current version is 5.0.5.0,
-        // you can set "incremental" to 5050. And when you have an upgrade available,
-        // set the "incremental" of that ROM to 5051 (or any number higher than 5050).
-        // ROM Manager will then figure out that an upgrade is available and let
-        // the user know.
-        incremental: 5060,
-        // The value of ro.modversion in /system/build.prop. Your ROM should set this
-        // in that build.prop so that ROM Manager can figure out what ROM is currently running.
-        // This is optional however, you don't need this line here or in /system/build.prop
-        // if you don't want to use OTA updates.
-        modversion: "CyanogenMod-5.0.6-Droid",
-        // This is a list of mirrors for your ROM. These URLs should have unique filenames per version!
-        // Ie, don't use a zip with the same name as that of a previous version with a new version.
-        // Otherwise ROM Manager will continue using the old cached zip on the SD card.
-        urls:
-        [
-        "http://www.droidaftermarket.com/koush/motorola/sholes/cyanogen_sholes-ota-eng.koush_5.0.6.zip",
-        "http://koush.romraid.com/motorola/sholes/cyanogen_sholes-ota-eng.koush_5.0.6.zip"
-        ],
-        // Optional addons (you don't need this if you dont have any)
-        addons:
-        [
-        {
-            name: "Google Apps",
-            urls:
-            [
-            "http://chris.soyars.com/gapps-passion-EPE54B-signed.zip",
-            "http://briancrook.ca/android/nexus/gapps/gapps-passion-EPE54B-signed.zip"
-            ]
-        }
-        ]
-    },
-
-
-    {
-        // Display name of the rom
         name: "CyanogenMod 5.0.5.7",
         // Display summary of the rom
         summary: "Stable",

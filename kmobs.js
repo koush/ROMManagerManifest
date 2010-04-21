0e5f5f9453a3358f222fb9cc3b490456916d6579
kmobs <persiansown@gmail.com>
2010-04-21 14:42:16 -0500

    4/21 850mV kernel update

--- kmobs.js
+++ kmobs.js
@@ -4,21 +4,22 @@
     roms:
     [
     {
+        name: "850mV Kernel",
+        summary: "Experimental - May not boot - 4/21",
+        device: "passion",
+        urls:
+        [
+        "http://kmobs.scepterr.info/kernels/signed-uv850-421.zip",
+        ]
+    },
+    {
+
         name: "925mV Kernel",
         summary: "Stable - 4/20",
         device: "passion",
         urls:
         [
         "http://kmobs.scepterr.info/kernels/signed-uv925.zip",
-        ]
-    },
-    {
-        name: "850mV Kernel",
-        summary: "Experimental - Device may not boot - 4/20",
-        device: "passion",
-        urls:
-        [
-        "http://kmobs.scepterr.info/kernels/signed-uv850-420.zip",
         ]
     }
     ]

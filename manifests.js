26ac698fabba27c56885b16ebbe6f37b8f201c6e
Koushik Dutta <koushd@gmail.com>
2010-04-21 12:58:16 -0700

    Add ArmyRanger251

--- manifests.js
+++ manifests.js
@@ -202,6 +202,16 @@
            },
     },
     {
+           developer: "ArmyRanger251",
+           id: "ArmyRanger251",
+           summary: "Themes for Cyanogen on the Droid.",
+           manifest: "http://theandroidspot.com/files/ArmyRanger251/ArmyRanger251.js",
+           roms:
+           {
+                sholes: true,
+           },
+    },
+    {
         developer: "PMilzie",
         id: "PMilzie",
         summary: "HexFusion Theme for Droid CyanogenMod",

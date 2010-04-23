e26bfa78ad9c2403543078a478347d27a8e4939d
Brian Crook <brian.w.crook@gmail.com>
2010-04-23 10:37:26 -0400

    Added Reaper to the manifests file. Issue 10

--- manifests.js
+++ manifests.js
@@ -261,9 +261,19 @@
         summary: "Great themes and more :)",
         manifest: "http://www.alldroid.org/themes/motorola/droid/krazykrivda/manifest.js",
         roms:
-    {
+        {
             sholes: true
+        },
     },
+    {
+       developer: "ArmyRanger251",
+       id: "ArmyRanger251",
+       summary: "Themes for Cyanogen on the Droid.",
+       manifest: "http://theandroidspot.com/files/ArmyRanger251/ArmyRanger251.js",
+       roms:
+       {
+            sholes: true,
+       },
     }
     ]
 }

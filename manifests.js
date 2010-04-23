475a6b2eaecc164d1867bb85bcd0bec56f42a454
Brian Crook <brian.w.crook@gmail.com>
2010-04-22 23:04:37 -0400

    krazykrivda to manifests.js

--- manifests.js
+++ manifests.js
@@ -254,6 +254,16 @@
         {
             passion: true
         },
+    },
+	 {
+    	  developer: "KrazyKrivda",
+        id: "krazykrivda",
+        summary: "Great themes and more :)",
+        manifest: "http://www.alldroid.org/themes/motorola/droid/krazykrivda/manifest.js",
+        roms:
+    {
+            sholes: true
+    },
     }
     ]
 }

5cc4ba4674e5203ee3a62dc25ad124c4add0cda4
Brian Crook <brian.w.crook@gmail.com>
2010-04-22 22:39:56 -0400

    Issue 10: Reaper Theme

---
+++ reaper.js
@@ -0,0 +1,10 @@
+{
+       developer: "ArmyRanger251",
+       id: "ArmyRanger251",
+       summary: "Themes for Cyanogen on the Droid.",
+       manifest: "http://theandroidspot.com/files/ArmyRanger251/ArmyRanger251.js",
+       roms:
+       {
+            sholes: true,
+       },
+}

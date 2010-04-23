402789d4cd511d8a294e15258d3fdd6ddd7c9d96
Brian Crook <brian.w.crook@gmail.com>
2010-04-22 22:44:44 -0400

    Issue 9: Krazy Krivda Theme

---
+++ krazykrivda.js
@@ -0,0 +1,10 @@
+{
+    developer: "KrazyKrivda",
+    id: "krazykrivda",
+    summary: "Great themes and more :)",
+    manifest: "http://www.alldroid.org/themes/motorola/droid/krazykrivda/manifest.js",
+    roms:
+    {
+        sholes: true
+    },
+}

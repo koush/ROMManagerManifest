7706db77c992fb9e21c0adb1b28ef5e22b016498
Koushik Dutta <koushd@gmail.com>
2010-04-29 10:15:18 -0700

    use su 2.0.3

--- all.js
+++ all.js
@@ -4,32 +4,32 @@
     roms:
     [
     {
-        name: "Superuser 2.0.2",
+        name: "Superuser 2.0.3",
         summary: "Experimental. Written by zinx and ChainsDD.",
         developer: true,
         device: "passion",
         urls:
         [
-        "http://koush.romraid.com//common/su-2.0.2-signed.zip",
-        "http://www.droidaftermarket.com/koush//common/su-2.0.2-signed.zip",
-        "http://droidk.macleodweb.net//common/su-2.0.2-signed.zip",
-        "http://android.antbox.org/koush/common/su-2.0.2-signed.zip",
-        "http://www.thekilpatrickproject.com/downloads/koush//common/su-2.0.2-signed.zip"
+        "http://koush.romraid.com//common/su-2.0.3-signed.zip",
+        "http://www.droidaftermarket.com/koush//common/su-2.0.3-signed.zip",
+        "http://droidk.macleodweb.net//common/su-2.0.3-signed.zip",
+        "http://android.antbox.org/koush/common/su-2.0.3-signed.zip",
+        "http://www.thekilpatrickproject.com/downloads/koush//common/su-2.0.3-signed.zip"
         ],
     },
     
     {
-        name: "Superuser 2.0.2",
+        name: "Superuser 2.0.3",
         summary: "Experimental. Written by zinx and ChainsDD.",
         developer: true,
         device: "sholes",
         urls:
         [
-        "http://koush.romraid.com//common/su-2.0.2-signed.zip",
-        "http://www.droidaftermarket.com/koush//common/su-2.0.2-signed.zip",
-        "http://droidk.macleodweb.net//common/su-2.0.2-signed.zip",
-        "http://android.antbox.org/koush/common/su-2.0.2-signed.zip",
-        "http://www.thekilpatrickproject.com/downloads/koush//common/su-2.0.2-signed.zip"
+        "http://koush.romraid.com//common/su-2.0.3-signed.zip",
+        "http://www.droidaftermarket.com/koush//common/su-2.0.3-signed.zip",
+        "http://droidk.macleodweb.net//common/su-2.0.3-signed.zip",
+        "http://android.antbox.org/koush/common/su-2.0.3-signed.zip",
+        "http://www.thekilpatrickproject.com/downloads/koush//common/su-2.0.3-signed.zip"
         ],
     }    
     ]

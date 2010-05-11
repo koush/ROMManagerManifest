6b62a33b6022ff3ea07ce2139ba0b7db2804bfe9
Brian Crook <brian.w.crook@gmail.com>
2010-05-11 02:37:37 -0400

    Add DS kernels and recent apps

--- pershoot.js
+++ pershoot.js
@@ -3,6 +3,9 @@
     homepage: "http://droidbasement.com/db-blog/",
     roms:
     [
+//**************************************
+//**********Nexus**************
+//**************************************
 //CyanogenMOD 5.0.7-t1
     {
         name: "Kernel 2.6.33.3 – OC-UV-BTN-AUD-RAM-NEON_FP (1.113GHZ)",
@@ -70,6 +73,112 @@
         urls:
         [
         "http://droidbasement.com/nexus/mods/15recent-avlock-cm5.0.6-043010.zip",
+        ]
+    },
+//**************************************
+//**********Dream/Sapphire**************
+//**************************************
+//32B - Sapphire
+    {
+        name: "Kernel 2.6.33.3, OC-UV-FP – Dream/Sapphire",
+        summary: "For CM-5.0.7 DS-test3",
+        device: "sapphire",
+        product: "Pershoot's Kernel",
+        incremental: 051010,
+        urls:
+        [
+        "http://droidbasement.com/g1/kernels/6/boot-cm_2633.3_ds_32b-oc-uv-fp-051010.zip",
+        ]
+    },
+    {
+        name: "Kernel 2.6.33.3, OC-UV-XTRA-FP – Dream/Sapphire",
+        summary: "For CM-5.0.7 DS-test3: XTRA",
+        device: "sapphire",
+        product: "Pershoot's Kernel",
+        incremental: 051010,
+        urls:
+        [
+        "http://droidbasement.com/g1/kernels/6/boot-cm_2633.3_ds_32b-oc-uv-xtra-fp-051010.zip",
+        ]
+    },
+//32B - Dream
+    {
+        name: "Kernel 2.6.33.3, OC-UV-FP – Dream/Sapphire",
+        summary: "For CM-5.0.7 DS-test3",
+        device: "dream",
+        product: "Pershoot's Kernel",
+        incremental: 051010,
+        urls:
+        [
+        "http://droidbasement.com/g1/kernels/6/boot-cm_2633.3_ds_32b-oc-uv-fp-051010.zip",
+        ]
+    },
+    {
+        name: "Kernel 2.6.33.3, OC-UV-XTRA-FP – Dream/Sapphire",
+        summary: "For CM-5.0.7 DS-test3: XTRA",
+        device: "dream",
+        product: "Pershoot's Kernel",
+        incremental: 051010,
+        urls:
+        [
+        "http://droidbasement.com/g1/kernels/6/boot-cm_2633.3_ds_32b-oc-uv-xtra-fp-051010.zip",
+        ]
+    },
+//32A - Magic
+    {
+        name: "Kernel 2.6.33.3, OC-UV-FP – Dream/Sapphire",
+        summary: "For CM-5.0.7 DS-test3",
+        device: "magic",
+        product: "Pershoot's Kernel",
+        incremental: 051010,
+        urls:
+        [
+        "http://droidbasement.com/g1/kernels/6/boot-cm_2633.3_ds_32a-oc-uv-fp-051010.zip",
+        ]
+    },
+    {
+        name: "Kernel 2.6.33.3, OC-UV-XTRA-FP – Dream/Sapphire",
+        summary: "For CM-5.0.7 DS-test3: XTRA",
+        device: "magic",
+        product: "Pershoot's Kernel",
+        incremental: 051010,
+        urls:
+        [
+        "http://droidbasement.com/g1/kernels/6/boot-cm_2633.3_ds_32a-oc-uv-xtra-fp-051010.zip",
+        ]
+    },    
+//Recent Apps
+    {
+        name: "15 Recent Apps – CM5.0.7-T Series – Dream/Sapphire",
+        summary: "For CyanogenMod 5.0.7 DS-test3 only",
+        device: "sapphire",
+        product: "Pershoot's Extras",
+        incremental: 051010,
+        urls:
+        [
+        "http://droidbasement.com/g1/mods/15recent-cm5.0.7_t-ds-051010.zip",
+        ]
+    },    
+    {
+        name: "15 Recent Apps – CM5.0.7-T Series – Dream/Sapphire",
+        summary: "For CyanogenMod 5.0.7 DS-test3 only",
+        device: "dream",
+        product: "Pershoot's Extras",
+        incremental: 051010,
+        urls:
+        [
+        "http://droidbasement.com/g1/mods/15recent-cm5.0.7_t-ds-051010.zip",
+        ]
+    },    
+    {
+        name: "15 Recent Apps – CM5.0.7-T Series – Dream/Sapphire",
+        summary: "For CyanogenMod 5.0.7 DS-test3 only",
+        device: "magic",
+        product: "Pershoot's Extras",
+        incremental: 051010,
+        urls:
+        [
+        "http://droidbasement.com/g1/mods/15recent-cm5.0.7_t-ds-051010.zip",
         ]
     }
     ]

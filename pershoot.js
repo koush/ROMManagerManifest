68d65e8d9b1738af6e95c3dde6cf746759cae6c2
Brian Crook <brian.w.crook@gmail.com>
2010-05-11 02:14:21 -0400

    Add 5.0.6 kernels

--- pershoot.js
+++ pershoot.js
@@ -3,9 +3,10 @@
     homepage: "http://droidbasement.com/db-blog/",
     roms:
     [
+//CyanogenMOD 5.0.7-t1
     {
         name: "Kernel 2.6.33.3 – OC-UV-BTN-AUD-RAM-NEON_FP (1.113GHZ)",
-        summary: "For CM-5.0.7-test1 ONLY: OC-UV-BTN-AUD-RAM-NEON_FP",
+        summary: "For CM-5.0.7-test1 ONLY",
         device: "passion",
         product: "Pershoot's Kernel",
         incremental: 051010,
@@ -16,7 +17,7 @@
     },
     {
         name: "Kernel 2.6.33.3 – OC-UV-XTRA-BTN-AUD-RAM-NEON_FP (1.113GHZ)",
-        summary: "For CM-5.0.7-test1 ONLY: OC-UV-XTRA-BTN-AUD-RAM-NEON_FP",
+        summary: "For CM-5.0.7-test1 ONLY: XTRA",
         device: "passion",
         product: "Pershoot's Kernel",
         incremental: 051010,
@@ -25,6 +26,30 @@
         "http://droidbasement.com/nexus/kernels/ram/19/zImage-cm_2633.3-oc-uv-xtra-btn-aud-ram-neon_fp-051010",
         ]
     },
+//CyanogenMOD 5.0.6
+	{
+        name: "Kernel 2.6.33.3 – OC-UV-BTN-AUD-RAM-NEON_FP (1.113GHZ)",
+        summary: "For CM-5.0.6 ONLY",
+        device: "passion",
+        product: "Pershoot's Kernel",
+        incremental: 050710,
+        urls:
+        [
+        "http://droidbasement.com/nexus/kernels/ram/17/boot-cm_2633.3-oc-uv-btn-aud-ram-vfp_fp-050710.zip",
+        ]
+    },
+    {
+        name: "Kernel 2.6.33.3 – OC-UV-XTRA-BTN-AUD-RAM-NEON_FP (1.113GHZ)",
+        summary: "For CM-5.0.6 ONLY: XTRA",
+        device: "passion",
+        product: "Pershoot's Kernel",
+        incremental: 050710,
+        urls:
+        [
+        "http://droidbasement.com/nexus/kernels/ram/17/boot-cm_2633.3-oc-uv-xtra-btn-aud-ram-vfp_fp-050710.zip",
+        ]
+    },
+//Recent Apps
     {
         name: "15 Recent Apps – CM5.0.7-T Series",
         summary: "For CyanogenMod 5.0.7-test1 only",
@@ -35,6 +60,19 @@
         [
         "http://droidbasement.com/nexus/mods/15recent-cm5.0.7_t-n1-051010.zip",
         ]
+    },
+    {
+        name: "15 Recent Apps – CM5.0.6",
+        summary: "For CyanogenMod 5.0.6 only",
+        device: "passion",
+        product: "Pershoot's Extras",
+        incremental: 043010,
+        urls:
+        [
+        "http://droidbasement.com/nexus/mods/15recent-avlock-cm5.0.6-043010.zip",
+        ]
     }
     ]
 }
+
+

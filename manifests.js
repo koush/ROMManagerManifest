1314f1019eb82881d709bc0fbd1334b456ca97cb
Koushik Dutta <koushd@gmail.com>
2010-05-10 22:58:21 -0700

    pretty up the javascript

--- manifests.js
+++ manifests.js
@@ -267,17 +267,17 @@
         {
             sholes: true
         },
-    },    
+    },
     {
         developer: "Pershoot's Kernels",
         id: "pershoot",
-	free: true,
+        free: true,
         summary: "Custom kernels",
         manifest: "http://gh-pages.clockworkmod.com/ROMManagerManifest/pershoot.js",
         roms:
         {
             passion: true
-        },        
+        },
     }
     ]
 }

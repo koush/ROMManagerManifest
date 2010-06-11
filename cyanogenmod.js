4bef00a31676e241f52e9e49d256719a8a25fb58
Koushik Dutta <koushd@gmail.com>
2010-06-11 13:03:20 -0700

    better gapps;

--- cyanogenmod.js
+++ cyanogenmod.js
@@ -1230,12 +1230,12 @@
             name: "Google Apps",
             urls:
             [
-            "http://koush.romraid.com//common/gapps-passion-EPE54B-signed.zip",
-            "http://www.droidaftermarket.com/koush//common/gapps-passion-EPE54B-signed.zip",
-            "http://droidk.macleodweb.net//common/gapps-passion-EPE54B-signed.zip",
-            "http://android.antbox.org/koush//common/gapps-passion-EPE54B-signed.zip",
-            "http://www.thekilpatrickproject.com/downloads/koush//common/gapps-passion-EPE54B-signed.zip",
-            "http://briancrook.ca/android/nexus/gapps/gapps-passion-EPE54B-signed.zip"
+            "http://www.droidaftermarket.com/koush//common/gapps-EPE54B-signed.zip",
+            "http://droidk.macleodweb.net//common/gapps-EPE54B-signed.zip",
+            "http://android.antbox.org/koush//common/gapps-EPE54B-signed.zip",
+            "http://www.thekilpatrickproject.com/downloads/koush//common/gapps-EPE54B-signed.zip",
+            "http://www.clockworkmod.com/download//common/gapps-EPE54B-signed.zip",
+            "http://koush.tandtgaming.com//common/gapps-EPE54B-signed.zip"
             ]
         }
         ]

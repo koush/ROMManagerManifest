1bb1259062dcf62c542bc36fcdea683ead589389
Your Name <brian.w.crook@gmail.com>
2010-06-19 15:34:12 -0400

    New gapps

--- cyanogenmod.js
+++ cyanogenmod.js
@@ -831,12 +831,7 @@
             name: "Google Apps",
             urls:
             [
-            "http://koush.romraid.com//common/gapps-passion-EPE54B-signed.zip",
-            "http://www.droidaftermarket.com/koush//common/gapps-passion-EPE54B-signed.zip",
-            "http://droidk.macleodweb.net//common/gapps-passion-EPE54B-signed.zip",
-            "http://android.antbox.org/koush//common/gapps-passion-EPE54B-signed.zip",
-            "http://www.thekilpatrickproject.com/downloads/koush//common/gapps-passion-EPE54B-signed.zip",
-            "http://briancrook.ca/android/nexus/gapps/gapps-passion-EPE54B-signed.zip"
+				"http://briancrook.ca/android/nexus/gapps/gapps-passion-EPF30-signed.zip"
             ]
         }
         ]

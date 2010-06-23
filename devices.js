af849fae69299f58acefedd1a710cece6c75b843
Koushik Dutta <koushd@gmail.com>
2010-06-22 19:03:10 -0700

    workaround for unable to format sdext

--- devices.js
+++ devices.js
@@ -1,7 +1,7 @@
 {
     recovery_url: "http://www.droidaftermarket.com/koush/recoveries/recovery-clockwork-%s-%s.img",
     recovery_zip_url: "http://www.droidaftermarket.com/koush/recoveries/recovery-clockwork-%s-%s.zip",
-    version: "2.0.1.0",
+    version: "2.0.1.2",
 
     legacy:
     [

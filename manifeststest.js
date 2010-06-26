a028b4a2f540237a435c4ce9a1b58e4bbe1f4f8d
Koushik Dutta <koushd@gmail.com>
2010-06-26 01:23:32 -0700

    test

--- manifeststest.js
+++ manifeststest.js
@@ -5,7 +5,7 @@
         "recovery_zip_url": "http://www.droidaftermarket.com/koush/recoveries/recovery-clockwork-%s-%s.zip",
         "version": "2.0.1.3",
 
-        legacy:
+        "legacy":
         [
         "2.0.1.4",
         "2.0.1.3",
@@ -41,149 +41,149 @@
         [
 
         {
-            init: "init.sholes.rc",
-            key: "sholes",
-            name: "Motorola Droid (CDMA)",
-            version: "1.8.1.2",
+            "init": "init.sholes.rc",
+            "key": "sholes",
+            "name": "Motorola Droid (CDMA)",
+            "version": "1.8.1.2",
             alternate_recovery:
             {
-                name: "SPRecovery 0.99.3b",
+                "name": "SPRecovery 0.99.3b",
                 url: "http://downloads.droidmod.org/recovery-0.99.3b.img"
             }
         },
 
         {
-            init: "init.sapphire.rc",
-            key: "sapphire",
-            name: "Ion/MyTouch 3G (GSM)",
-            version: "1.8.1.0",
+            "init": "init.sapphire.rc",
+            "key": "sapphire",
+            "name": "Ion/MyTouch 3G (GSM)",
+            "version": "1.8.1.0",
             alternate_recovery:
             {
-                name: "RA Recovery v1.7.0G",
+                "name": "RA Recovery v1.7.0G",
                 url: "http://files.androidspin.com/downloads.php?dir=amon_ra/RECOVERY/&file=recovery-RA-sapphire-v1.7.0G.img"
 
             }
         },
 
         {
-            init: "init.sapphire.rc",
-            key: "magic",
-            name: "HTC Magic (GSM)",
-            version: "1.8.1.5",
+            "init": "init.sapphire.rc",
+            "key": "magic",
+            "name": "HTC Magic (GSM)",
+            "version": "1.8.1.5",
             alternate_recovery:
             {
-                name: "RA Recovery v1.7.0H",
+                "name": "RA Recovery v1.7.0H",
                 url: "http://files.androidspin.com/downloads.php?dir=amon_ra/RECOVERY/&file=recovery-RA-sapphire-v1.7.0H.img"
             }
         },
 
         {
-            init: "init.trout.rc",
-            key: "dream",
-            name: "HTC Dream (GSM)",
-            version: "1.8.1.0",
+            "init": "init.trout.rc",
+            "key": "dream",
+            "name": "HTC Dream (GSM)",
+            "version": "1.8.1.0",
             alternate_recovery:
             {
-                name: "RA Recovery v1.7.0",
+                "name": "RA Recovery v1.7.0",
                 url: "http://files.androidspin.com/downloads.php?dir=amon_ra/RECOVERY/&file=recovery-RA-dream-v1.7.0.img"
             }
         },
 
         {
-            init: "init.mahimahi.rc",
-            key: "passion",
-            name: "Google Nexus One (GSM)",
-            version: "1.8.1.0",
+            "init": "init.mahimahi.rc",
+            "key": "passion",
+            "name": "Google Nexus One (GSM)",
+            "version": "1.8.1.0",
             alternate_recovery:
             {
-                name: "RA Recovery v1.7.0.1",
+                "name": "RA Recovery v1.7.0.1",
                 url: "http://files.androidspin.com/downloads.php?dir=amon_ra/RECOVERY/&file=recovery-RA-nexus-v1.7.0.1.img"
             }
         },
 
         {
-            init: "init.hero.rc",
-            key: "hero",
-            name: "HTC Hero (GSM)",
-            version: "1.8.1.0",
+            "init": "init.hero.rc",
+            "key": "hero",
+            "name": "HTC Hero (GSM)",
+            "version": "1.8.1.0",
             alternate_recovery:
             {
-                name: "RA Recovery v1.6.2",
+                "name": "RA Recovery v1.6.2",
                 url: "http://www.androidspin.com/downloads.php?dir=amon_ra/RECOVERY/&file=recovery-RA-hero-v1.6.2.img"
             }
         },
 
         {
-            init: "init.heroc.rc",
-            key: "heroc",
-            name: "HTC Hero (CDMA)",
-            version: "1.8.1.0",
+            "init": "init.heroc.rc",
+            "key": "heroc",
+            "name": "HTC Hero (CDMA)",
+            "version": "1.8.1.0",
             alternate_recovery:
             {
-                name: "RA Recovery v1.6.2",
+                "name": "RA Recovery v1.6.2",
                 url: "http://www.androidspin.com/downloads.php?dir=amon_ra/RECOVERY/&file=recovery-RA-heroc-v1.6.2.img"
             }
         },
 
         {
-            init: "init.supersonic.rc",
-            key: "supersonic",
-            name: "HTC Evo (CDMA)",
-            version: "1.8.1.9",
+            "init": "init.supersonic.rc",
+            "key": "supersonic",
+            "name": "HTC Evo (CDMA)",
+            "version": "1.8.1.9",
             alternate_recovery:
             {
-                name: "RA Recovery v1.7.0.1",
+                "name": "RA Recovery v1.7.0.1",
                 url: "http://files.androidspin.com/downloads.php?dir=amon_ra/RECOVERY/&file=recovery-RA-evo-v1.7.0.1.img"
             }
         },
 
         {
-            init: "init.legend.rc",
-            key: "legend",
-            name: "HTC Legend",
-            version: "1.8.1.7",
-            readonly_recovery: true
+            "init": "init.legend.rc",
+            "key": "legend",
+            "name": "HTC Legend",
+            "version": "1.8.1.7",
+            "readonly_recovery": true
         },
 
         {
-            init: "init.bravo.rc",
-            key: "bravo",
-            name: "HTC Desire",
-            version: "2.0.1.4",
-            readonly_recovery: true
+            "init": "init.bravo.rc",
+            "key": "bravo",
+            "name": "HTC Desire",
+            "version": "2.0.1.4",
+            "readonly_recovery": true
         },
 
         {
-            init: "init.qcom.rc",
-            key: "pulsemini",
-            name: "Huawei Pulse Mini",
-            version: "1.8.1.0"
+            "init": "init.qcom.rc",
+            "key": "pulsemini",
+            "name": "Huawei Pulse Mini",
+            "version": "1.8.1.0"
         },
 
         {
-            init: "init.inc.rc",
-            key: "inc",
-            name: "HTC Incredible (CDMA)",
-            version: "2.0.0.2",
-            readonly_recovery: true
+            "init": "init.inc.rc",
+            "key": "inc",
+            "name": "HTC Incredible (CDMA)",
+            "version": "2.0.0.2",
+            "readonly_recovery": true
         },
 
         {
-            init: "init.liberty.rc",
-            key: "liberty",
-            name: "HTC Aria (GSM)",
-            version: "2.0.1.3",
-            readonly_recovery: true
+            "init": "init.liberty.rc",
+            "key": "liberty",
+            "name": "HTC Aria (GSM)",
+            "version": "2.0.1.3",
+            "readonly_recovery": true
         },
     
         {
-            init: "init.desirec.rc",
-            key: "desirec",
-            name: "Droid Eris (CDMA)",
-            version: "1.8.1.0",
+            "init": "init.desirec.rc",
+            "key": "desirec",
+            "name": "Droid Eris (CDMA)",
+            "version": "1.8.1.0",
             alternate_recovery:
             {
-                name: "RA Recovery v1.6.2",
+                "name": "RA Recovery v1.6.2",
                 url: "http://www.androidspin.com/downloads.php?dir=amon_ra/RECOVERY/&file=recovery-RA-eris-v1.6.2.img"
             }
         }

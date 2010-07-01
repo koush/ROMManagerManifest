1afa5188d277a8cb12ed51c041c48f0898d97ffb
Your Name <brian.w.crook@gmail.com>
2010-07-01 08:36:06 -0400

    crunchysoft test manifest

---
+++ crunchysoft.js
@@ -0,0 +1,71 @@
+	{
+		version: 1,
+		homepage: "http://crunchysoft.pocketdevelopers.com/",
+		donate: "https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&amp;hosted_button_id=T6GGP7T6NVV6Q",
+		roms:
+		[
+		{
+		    name: "Buufed 2.0.1",
+		    summary: "Stable",
+		    device: "heroc",
+		    product: "Buufed",
+		    incremental: 201,
+		    modversion: "Buufed-2.0.1",
+		    screenshots:
+		    [
+		    "http://crunchysoft.pocketdevelopers.com/img/ss/rom1.png"",
+		    "http://crunchysoft.pocketdevelopers.com/img/ss/rom2.png"",
+		    "http://crunchysoft.pocketdevelopers.com/img/ss/rom3.png"",
+		    "http://crunchysoft.pocketdevelopers.com/img/ss/rom4.png"",
+		    "http://crunchysoft.pocketdevelopers.com/img/ss/rom5.png"",
+		    "http://crunchysoft.pocketdevelopers.com/img/ss/rom6.png"",
+		    "http://crunchysoft.pocketdevelopers.com/img/ss/rom7.png""
+		    ],
+		    urls:
+		    [
+		    "http://crunchysoft.pocketdevelopers.com/crunchyroms/Buufed-2.0.1.zip",
+		    "https://dl-web.dropbox.com/s/5kojzcgf9n0u4ae/Buufed-2.0.1.zip?dl"
+		    ],
+		    addons:
+		    [
+		    {
+		        name: "FCFix"
+		        urls:
+		        [
+		        "http://crunchysoft.pocketdevelopers.com/misc/patch/2.0.1-FC-Fixes.zip",
+		        ]
+		    },
+		    {
+		        name: "VisualVoicemail"
+		        urls:
+		        [
+		        "http://crunchysoft.pocketdevelopers.com//misc/patch/2.0.1-VVM-Fix.zip",
+		        ]
+		    }
+		    ]
+		},
+		// And here's another, older ROM!
+		{
+		    name: "Buufed 1.7.2",
+		    summary: "Stable",
+		    device: "heroc",
+		    product: "Buufed",
+		    incremental: 172,
+		    modversion: "Buufed-1.7.2",
+		    urls:
+		    [
+		    "http://crunchysoft.pocketdevelopers.com/crunchyroms/Buufed-1.7.2.zip",
+		    ],
+		    addons:
+		    [
+		    {
+		        name: "VisualVoicemail"
+		        urls:
+		        [
+		        "http://crunchysoft.pocketdevelopers.com/misc/SprintVM.zip",
+		        ]
+		    }
+		    ]
+		}
+		]
+	}

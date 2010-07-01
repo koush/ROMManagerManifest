584c9bc5d9612837ea7cf3272e71eb4b0f8b8fbb
Koushik Dutta <koushd@gmail.com>
2010-07-01 07:37:50 -0700

    Revert "Updated manifest fo crunchysoft"
    This reverts commit ff5c3e3e69e80be9aceb3e76b96e7deb33efb00f.

--- manifests.js
+++ manifests.js
@@ -451,12 +451,11 @@
 		developer: "CrunchySoft",
 		id: "crunchybutternut",
 		summary: "Fast and fully themed ROMs for the CDMA Hero by crunchybutternut.",
-	    free: true,
 		manifest: "http://crunchysoft.pocketdevelopers.com/ROMManagerManifest/crunchysoft.js",
 		roms:
 		{
-		    heroc: true
-
+		    heroc: true,
+		    free: true,
 		}
 	},
 

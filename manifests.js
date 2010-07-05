0e3b8fe7ab23d517c02594a631a48dbd46532c72
Your Name <brian.w.crook@gmail.com>
2010-07-05 19:37:50 -0400

    Add cvpcs sapphire

--- manifests.js
+++ manifests.js
@@ -484,7 +484,18 @@
             bravo: true
         }
     },
-    
+	{
+
+		developer: "cvpcs",
+		id: "cvpcs",
+		summary: "Source-based mods made by CVPCS",
+		manifest: "http://sapphire.cvpcs.org/files/other/cvpcs-rmm.js",
+		free: true,
+		roms:
+		{
+			sholes: true
+		}
+	},    
     {
 
         developer: "InsaneNemesis",

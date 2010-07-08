24291fcfd6d578ca7ff06569893f9a2b3ddd0f12
Koushik Dutta <koushd@gmail.com>
2010-07-08 13:04:49 -0700

    su2.2

--- all.js
+++ all.js
@@ -1,36 +1,21 @@
 {
     version: 1,
 
+    mirrors:
+    [
+    "http://droidk.macleodweb.net/",
+    "http://android.antbox.org/koush/",
+    "http://www.thekilpatrickproject.com/downloads/koush/",
+    "http://www.clockworkmod.com/download/",
+    "http://koush.tandtgaming.com/"
+    ],
+
     roms:
     [
     {
-        name: "Superuser 2.0.3",
-        summary: "Experimental. Written by zinx and ChainsDD.",
-        developer: true,
-        device: "passion",
-        urls:
-        [
-        "http://koush.romraid.com//common/su-2.0.3-signed.zip",
-        "http://www.droidaftermarket.com/koush//common/su-2.0.3-signed.zip",
-        "http://droidk.macleodweb.net//common/su-2.0.3-signed.zip",
-        "http://android.antbox.org/koush/common/su-2.0.3-signed.zip",
-        "http://www.thekilpatrickproject.com/downloads/koush//common/su-2.0.3-signed.zip"
-        ]
-    },
-    
-    {
-        name: "Superuser 2.0.3",
-        summary: "Experimental. Written by zinx and ChainsDD.",
-        developer: true,
-        device: "sholes",
-        urls:
-        [
-        "http://koush.romraid.com//common/su-2.0.3-signed.zip",
-        "http://www.droidaftermarket.com/koush//common/su-2.0.3-signed.zip",
-        "http://droidk.macleodweb.net//common/su-2.0.3-signed.zip",
-        "http://android.antbox.org/koush/common/su-2.0.3-signed.zip",
-        "http://www.thekilpatrickproject.com/downloads/koush//common/su-2.0.3-signed.zip"
-        ]
+        name: "Superuser 2.2",
+        device: "all",
+        file: "recoveries/su-2.2-ef-signed.zip"
     }    
     ]
 }

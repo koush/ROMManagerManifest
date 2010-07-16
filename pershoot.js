ec53d504fb290b6c795c99033a1af6d748e5cf5d
Your Name <brian.w.crook@gmail.com>
2010-07-15 21:38:34 -0400

    Updated ADW and kernels

--- pershoot.js
+++ pershoot.js
@@ -7,47 +7,47 @@
 //**********ADW Launcher**************
 //**************************************
     {
-        name: "ADW Launcher 0.8",
-        summary: "ADW Launcher (almostNexus mod) 0.8 (.6), by Anderwebs: June 19",
+        name: "ADW Launcher 0.9",
+        summary: "ADW Launcher (almostNexus mod) 0.9, by Anderwebs: July 1",
         device: "passion",
         product: "Anderweb's Launcher",
-        incremental: 061910,
+        incremental: 070110,
         urls:
         [
-        "http://droidbasement.com/nexus/mods/adwlauncher_0.8-n1-061910.zip",
+        "http://droidbasement.com/nexus/mods/adwlauncher_0.9-n1-070110.zip",
         ]
     },
     {
-        name: "ADW Launcher 0.8",
-        summary: "ADW Launcher (almostNexus mod) 0.8 (.6), by Anderwebs: June 19",
+        name: "ADW Launcher 0.9",
+        summary: "ADW Launcher (almostNexus mod) 0.9, by Anderwebs: July 1",
         device: "dream",
         product: "Anderweb's Launcher",
-        incremental: 061910,
+        incremental: 070110,
         urls:
         [
-        "http://droidbasement.com/g1/mods/adwlauncher_0.8-ds-061910.zip",
+        "http://droidbasement.com/g1/mods/adwlauncher_0.9-ds-070110.zip",
         ]
     },
     {
-        name: "ADW Launcher 0.8",
-        summary: "ADW Launcher (almostNexus mod) 0.8 (.6), by Anderwebs: June 19",
+        name: "ADW Launcher 0.9",
+        summary: "ADW Launcher (almostNexus mod) 0.9, by Anderwebs: July 1",
         device: "magic",
         product: "Anderweb's Launcher",
-        incremental: 061910,
+        incremental: 070110,
         urls:
         [
-        "http://droidbasement.com/g1/mods/adwlauncher_0.8-ds-061910.zip",
+        "http://droidbasement.com/g1/mods/adwlauncher_0.9-ds-070110.zip",
         ]
     },
     {
-        name: "ADW Launcher 0.8",
-        summary: "ADW Launcher (almostNexus mod) 0.8 (.6), by Anderwebs: June 19",
+        name: "ADW Launcher 0.9",
+        summary: "ADW Launcher (almostNexus mod) 0.9, by Anderwebs: July 1",
         device: "sapphire",
         product: "Anderweb's Launcher",
-        incremental: 061910,
+        incremental: 070110,
         urls:
         [
-        "http://droidbasement.com/g1/mods/adwlauncher_0.8-ds-061910.zip",
+        "http://droidbasement.com/g1/mods/adwlauncher_0.9-ds-070110.zip",
         ]
     },
 //**************************************
@@ -55,81 +55,96 @@
 //**************************************
     {
         name: "Kernel 2.6.35 – OC-UV-BTN-AUD-RAM-NEON_FP (1.113GHZ)",
-        summary: "Android 2.2 (Froyo): June 29",
+        summary: "Android 2.2 (Froyo): July 2",
         device: "passion",
         product: "Pershoot's Kernel",
-        incremental: 062910,
+        incremental: 070210,
         urls:
         [
-        "http://droidbasement.com/nexus/kernels/ram/2635/2/boot-cm_2635_rc3_froyo-oc-uv-btn-aud-ram-neon_fp-062910.zip",
+        "http://droidbasement.com/nexus/kernels/ram/2635/4/boot-cm_2635_rc3_froyo-oc-uv-btn-aud-ram-neon_fp-070210.zip",
         ]
     },
     {
-        name: "Kernel 2.6.35 – OC-UV-XTRA-BTN-AUD-RAM-NEON_FP (1.113GHZ)",
-        summary: "Android 2.2 (Froyo) XTRA: June 29",
+        name: "Kernel 2.6.35 – OC-UV-BTN-AUD-RAM-NEON_FP (1.113GHZ) XTRA",
+        summary: "Android 2.2 (Froyo): July 2",
         device: "passion",
         product: "Pershoot's Kernel",
-        incremental: 062910,
+        incremental: 070210,
         urls:
         [
-        "http://droidbasement.com/nexus/kernels/ram/2635/2/boot-cm_2635_rc3_froyo-oc-uv-xtra-btn-aud-ram-neon_fp-062910.zip",
+        "http://droidbasement.com/nexus/kernels/ram/2635/4/boot-cm_2635_rc3_froyo-oc-uv-xtra-btn-aud-ram-neon_fp-070210.zip",
+        ]
+    },
+//5.0.8
+    {
+        name: "Kernel 2.6.35 – OC-UV-BTN-AUD-RAM-NEON_FP (1.113GHZ)",
+        summary: "CM5.0.5.x, CM5.0.7.x and CM5.0.8-T Series/CM5.0.8: July 2",
+        device: "passion",
+        product: "Pershoot's Kernel",
+        incremental: 070210,
+        urls:
+        [
+        "http://droidbasement.com/nexus/kernels/ram/2635/4/boot-cm_2635_rc3-oc-uv-btn-aud-ram-neon_fp-070210.zip",
         ]
     },
     {
-        name: "Kernel 2.6.33.4 – OC-UV-BTN-AUD-RAM-NEON_FP (1.113GHZ)",
-        summary: "CM5.0.5.x, CM5.0.7.x and CM5.0.8-T Series: June 29",
+        name: "Kernel 2.6.35 – OC-UV-BTN-AUD-RAM-NEON_FP (1.113GHZ) XTRA",
+        summary: "CM5.0.5.x, CM5.0.7.x and CM5.0.8-T Series/CM5.0.8: July 2",
         device: "passion",
         product: "Pershoot's Kernel",
-        incremental: 062910,
+        incremental: 070210,
         urls:
         [
-        "http://droidbasement.com/nexus/kernels/ram/2635/2/boot-cm_2635_rc3-oc-uv-btn-aud-ram-neon_fp-062910.zip",
+        "http://droidbasement.com/nexus/kernels/ram/2635/4/boot-cm_2635_rc3-oc-uv-xtra-btn-aud-ram-neon_fp-070210.zip",
+        ]
+    },
+//.34
+    {
+        name: "Kernel 2.6.35 – OC-UV-BTN-AUD-RAM-NEON_FP (1.113GHZ)",
+        summary: "Android 2.2 (Froyo): July 13",
+        device: "passion",
+        product: "Pershoot's Kernel",
+        incremental: 071310,
+        urls:
+        [
+        "http://droidbasement.com/nexus/kernels/ram/38/boot-cm_2634.1-oc-uv-btn-aud-ram-neon_fp-071310.zip",
         ]
     },
     {
-        name: "Kernel 2.6.33.4 – OC-UV-XTRA-BTN-AUD-RAM-NEON_FP (1.113GHZ)",
-        summary: "CM5.0.5.x, CM5.0.7.x and CM5.0.8-T Series XTRA: June 29",
+        name: "Kernel 2.6.35 – OC-UV-BTN-AUD-RAM-NEON_FP (1.113GHZ) XTRA",
+        summary: "Android 2.2 (Froyo): July 13",
         device: "passion",
         product: "Pershoot's Kernel",
-        incremental: 062910,
+        incremental: 071310,
         urls:
         [
-        "http://droidbasement.com/nexus/kernels/ram/2635/2/boot-cm_2635_rc3_froyo-oc-uv-xtra-btn-aud-ram-neon_fp-062910.zip",
+        "http://droidbasement.com/nexus/kernels/ram/38/boot-cm_2634.1-oc-uv-xtra-btn-aud-ram-neon_fp-071310.zip",
+        ]
+    },
+//.34 5.0.x
+    {
+        name: "Kernel 2.6.34.1 – OC-UV-BTN-AUD-RAM-NEON_FP (1.113GHZ)",
+        summary: "CM5.0.5.x, CM5.0.7.x and CM5.0.8-T Series/CM5.0.8: July 13",
+        device: "passion",
+        product: "Pershoot's Kernel",
+        incremental: 071310,
+        urls:
+        [
+        "http://droidbasement.com/nexus/kernels/ram/38/boot-cm_2634.1-oc-uv-btn-aud-ram-neon_fp-071310.zip",
         ]
     },
     {
-        name: "Kernel 2.6.33.4 – OC-UV-BTN-AUD-RAM-VFP_FP (1.113GHZ)",
-        summary: "For CM-5.0.6: May 18",
+        name: "Kernel 2.6.34.1 – OC-UV-BTN-AUD-RAM-NEON_FP (1.113GHZ) XTRA",
+        summary: "CM5.0.5.x, CM5.0.7.x and CM5.0.8-T Series/CM5.0.8: July 13",
         device: "passion",
         product: "Pershoot's Kernel",
-        incremental: 051810,
+        incremental: 071310,
         urls:
         [
-        "http://droidbasement.com/nexus/kernels/ram/21/boot-cm_2633.4-oc-uv-btn-aud-ram-vfp_fp-051810.zip",
+        "http://droidbasement.com/nexus/kernels/ram/38/boot-cm_2634.1-oc-uv-xtra-btn-aud-ram-neon_fp-071310.zip",
         ]
     },
-    {
-        name: "Kernel 2.6.33.4 – OC-UV-XTRA-BTN-AUD-RAM-VFP_FP (1.113GHZ)",
-        summary: "For CM-5.0.6, XTRA: May 18",
-        device: "passion",
-        product: "Pershoot's Kernel",
-        incremental: 051810,
-        urls:
-        [
-        "http://droidbasement.com/nexus/kernels/ram/21/boot-cm_2633.4-oc-uv-xtra-btn-aud-ram-vfp_fp-051810.zip",
-        ]
-    },
-    {
-        name: "Old Wifi Driver",
-        summary: "For 2.6.33.4: June 19",
-        device: "passion",
-        product: "Pershoot's Kernel",
-        incremental: 061910,
-        urls:
-        [
-        "http://droidbasement.com/nexus/kernels/ram/28/wifi-OLD.zip",
-        ]
-    },
+
 //**************************************
 //**********Dream/Sapphire**************
 //**************************************

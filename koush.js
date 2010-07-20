d90d88dce5e2926052baab8f86d79fde667eac32
Koushik Dutta <koushd@gmail.com>
2010-07-19 21:38:04 -0700

    test4

--- koush.js
+++ koush.js
@@ -16,28 +16,10 @@
     [
 
     {
-        name: "CyanogenMod 6.0.0 test2",
+        name: "CyanogenMod 6.0.0 test4",
         summary: "Broken Camera",
         device: "inc",
-        url: "http://koush.tandtgaming.com//test/update-cm-6.0.0-Inc-test2-signed.zip",
-        screenshots:
-        [
-        "http://koush.tandtgaming.com//motorola/sholes/screeshot.png",
-        "http://koush.tandtgaming.com//motorola/sholes/screenshot2.png"
-        ],
-        addons:
-        [
-        {
-            name: "Google Apps",
-            url: "http://kanged.net/mirror/gapps/gapps-hdpi-FRF91-2-signed.zip"
-        }
-        ]
-    },
-    {
-        name: "CyanogenMod 6.0.0 test1",
-        summary: "Broken Camera",
-        device: "inc",
-        url: "http://koush.tandtgaming.com//test/update-cm-6.0.0-Inc-test1-signed.zip",
+        url: "http://koush.tandtgaming.com//test/update-cm-6.0.0-Inc-test4-signed.zip",
         screenshots:
         [
         "http://koush.tandtgaming.com//motorola/sholes/screeshot.png",

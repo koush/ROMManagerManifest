1f8eb4cd2fd76abc49d48c049de78cbddb8753d7
Koushik Dutta <koushd@gmail.com>
2010-07-26 18:45:30 -0700

    rc1

--- cyanogenmod.js
+++ cyanogenmod.js
@@ -859,8 +859,8 @@
         device: "bravo",
         product: "CyanogenModRC",
         incremental: 2,
-        modversion: "CyanogenMod-6.0.0-Bravo-RC2",
-        file: "htc/bravo/update-cm-6.0.0-bravo-RC2-signed.zip",
+        modversion: "CyanogenMod-6.0.0-Bravo-RC1",
+        file: "htc/bravo/update-cm-6.0.0-bravo-RC1-signed.zip",
         screenshots:
         [
         "http://koush.tandtgaming.com//motorola/sholes/screeshot.png",

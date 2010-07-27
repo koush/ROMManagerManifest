01ba205bf5b83a81d89b889bbd058fa19a88d8db
Koushik Dutta <koushd@gmail.com>
2010-07-27 09:26:29 -0700

    test8

--- koush.js
+++ koush.js
@@ -16,10 +16,10 @@
     [
 
     {
-        name: "CyanogenMod 6.0.0 test7",
+        name: "CyanogenMod 6.0.0 test8",
         summary: "Broken Camera",
         device: "inc",
-        url: "http://koush.tandtgaming.com//test/update-cm-6.0.0-Inc-test7-signed.zip",
+        url: "http://koush.tandtgaming.com//htc/inc/update-cm-6.0.0-Inc-test8-signed.zip",
         screenshots:
         [
         "http://koush.tandtgaming.com//motorola/sholes/screeshot.png",

992599e4862b5da8eefb1f4d4e82366fb576e31b
Koushik Dutta <koushd@gmail.com>
2010-07-28 12:35:41 -0700

    inc

--- devices.js
+++ devices.js
@@ -158,7 +158,7 @@
         init: "init.inc.rc",
         key: "inc",
         name: "HTC Incredible (CDMA)",
-        version: "2.0.2.1",
+        version: "2.5.0.4",
         readonly_recovery: true
     },
 

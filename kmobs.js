c09971188c9fb9fec32759f4fa9174be4c475e61
kmobs <persiansown@gmail.com>
2010-09-08 20:55:34 -0500

    Updated karananannanal

--- kmobs.js
+++ kmobs.js
@@ -5,12 +5,12 @@
     [
     {
         name: "950mV Kernel - for CM6",
-        summary: ".34.5 - Updated 8/29",
+        summary: ".35.4 - Updated 9/08",
         device: "passion",
 	screenshot: "http://web13.twitpic.com/img/118594009-1b6e629560f853477db77e8ed8f55170.4c1ec098-full.png",
         urls:
         [
-        "http://kanged.net/up/files/5/kernels/signed-August26-UV950.zip",
+        "http://kanged.net/up/files/5/kernels/signed-Sept8-UV950.zip",
         ]
     }
     ]

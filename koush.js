134d677c2fe8e0253ae9d8f8d04d5a9e881a6309
Koushik Dutta <koushd@gmail.com>
2010-09-24 18:22:51 -0700

    test

--- koush.js
+++ koush.js
@@ -7,31 +7,19 @@
     "http://android.antbox.org/koush/",
     "http://www.thekilpatrickproject.com/downloads/koush/",
     "http://www.clockworkmod.com/download/",
-    "http://koush.tandtgaming.com/"
+    "http://koush.tandtgaming.com/",
+    "http://koush.droid.co"
     ],
     
     // Your list of ROMs
     // =====HTC Incredible=====
     roms:
     [
-
     {
-        name: "CyanogenMod 6.0.0 test8",
-        summary: "Broken Camcorder",
+        name: "Test Kernel",
+        summary: "Test/stock",
         device: "inc",
-        url: "http://koush.tandtgaming.com//htc/inc/update-cm-6.0.0-Inc-test8-signed.zip",
-        screenshots:
-        [
-        "http://koush.tandtgaming.com//motorola/sholes/screeshot.png",
-        "http://koush.tandtgaming.com//motorola/sholes/screenshot2.png"
-        ],
-        addons:
-        [
-        {
-            name: "Google Apps",
-            url: "http://kanged.net/mirror/gapps/gapps-hdpi-FRF91-2-signed.zip"
-        }
-        ]
+        file: "/htc/inc/inc-test.zip"
     }
     ]
 }

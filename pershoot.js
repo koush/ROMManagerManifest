07496ba2819deaed815209ec72b79d806c17622f
Prashant Somashekar <prashsomash@gmail.com>
2010-10-23 10:02:09 -0400

    1023

--- pershoot.js
+++ pershoot.js
@@ -8,9 +8,9 @@
     //**************************************
     {
         name: "Kernel 2.6.35.7 – OC-UV-XTRA-BTN-AUD-RAM-NEON_FP",
-        summary: "1022 - Nexus",
+        summary: "1023 - Nexus",
         device: "passion",
-        url: "http://droidbasement.com/nexus/kernels/ram/2635/40/boot-cm_2635.7-oc-uv-xtra-btn-aud-ram-neon_fp-102210.zip"
+        url: "http://droidbasement.com/nexus/kernels/ram/2635/41/boot-cm_2635.7-oc-uv-xtra-btn-aud-ram-neon_fp-102310.zip"
     },
     //**************************************
     //**********Dream/Sapphire**************
@@ -19,20 +19,20 @@
     //32B - Dream/Sapphire
     {
         name: "Kernel 2.6.35.7 - OC-UV-XTRA-RAM-FP",
-        summary: "1022 - Dream/Sapphire (32B)",
+        summary: "1023 - Dream/Sapphire (32B)",
         devices:
         [
         "dream",
         "sapphire"
         ],
-        url: "http://droidbasement.com/g1/kernels/ram/2635/4/boot-cm_2635.7_ds_32b-oc-uv-xtra-ram-fp-102210.zip"
+        url: "http://droidbasement.com/g1/kernels/ram/2635/5/boot-cm_2635.7_ds_32b-oc-uv-xtra-ram-fp-102310.zip"
     },
     //32A - Magic
     {
         name: "Kernel 2.6.35.7 - OC-UV-XTRA-FP",
-        summary: "1022 - Dream/Sapphire (32A)",
+        summary: "1023 - Dream/Sapphire (32A)",
         device: "magic",
-        url: "http://droidbasement.com/g1/kernels/ram/2635/4/boot-cm_2635.7_ds_32a-oc-uv-xtra-ram-fp-102210.zip"
+        url: "http://droidbasement.com/g1/kernels/ram/2635/5/boot-cm_2635.7_ds_32a-oc-uv-xtra-ram-fp-102310.zip"
     }
     //END
     ]

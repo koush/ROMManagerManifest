a078bc622e5fd287f2f5edb410147a38520d09fb
Deodexed <Deodexed@gmail.com>
2010-10-27 14:27:17 -0400

    added deodexed rom

---
+++ deodexed.js
@@ -0,0 +1,12 @@
+{
+    
+    developer: "Deodexed Elements Series",
+    id: "Deodexed",
+    summary: "Elements Rom Series, Zinc Rom for the Samsung Fascinate",
+    manifest: "http://deodexed.com/ROMManagerManifest/deodexed_manifest.js",
+    roms:
+    {
+        SCH-I500-Fascinate: true
+    }
+}
+

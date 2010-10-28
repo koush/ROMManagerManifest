98b8f3a924adead0dd87b5dacc611865da6eb4e1
Deodexed <Deodexed@gmail.com>
2010-10-28 05:27:17 -0700

    Added Elements Rom Series, Zinc Rom for the Samsung Fascinate

--- manifests.js
+++ manifests.js
@@ -820,6 +820,16 @@
         {
             droidx: true
         }
+    },
+    {
+        developer: "Deodexed Elements Series",
+        id: "Deodexed",
+        summary: "Elements Rom Series, Zinc Rom for the Samsung Fascinate",
+        manifest: "http://deodexed.com/ROMManagerManifest/deodexed_manifest.js",
+        roms:
+        {
+           fascinate: true
+        }
     }
     ]
 }

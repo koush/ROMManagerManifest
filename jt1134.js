fc9254132f4d38db771ef14948a747f732016315
jt1134 <jt1134@gmail.com>
2010-11-02 05:05:48 +0800

    add jt1134 manifest

---
+++ jt1134.js
@@ -0,0 +1,40 @@
+{
+    version: 1,
+	donate: "http://bit.ly/a34JEE",
+    roms:
+    [
+    {
+        name: "Super Clean ROM for Samsung Fascinate",
+        summary: "Version 0.4 - Updated 10/30",
+        device: "fascinate",
+        addons:
+        [
+        {
+            name: "Puzzle Piece Lock Screen",
+            urls:
+            [
+            "http://deodexed.com/ROMManagerManifest/jt1134/puzzle_lock.zip",
+            ]
+        },
+        {
+            name: "Stock Fascinate Mms.apk",
+            urls:
+            [
+            "http://deodexed.com/ROMManagerManifest/jt1134/Mms.zip",
+            ]
+        },
+        {
+            name: "Verizon Visual VoiceMail",
+            urls:
+            [
+            "http://deodexed.com/ROMManagerManifest/jt1134/VVM.zip",
+            ]
+        }
+        ],
+        urls:
+        [
+        "http://deodexed.com/ROMManagerManifest/jt1134/clean-update-0.4.zip",
+        ]
+    }
+    ]
+}

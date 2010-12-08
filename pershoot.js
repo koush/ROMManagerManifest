bba334c934f4ed3f55335c7816ed320912a71571
Prashant Somashekar <prashsomash@gmail.com>
2010-12-07 23:01:14 -0500

    1207 - nexus/ds

--- pershoot.js
+++ pershoot.js
@@ -17,9 +17,9 @@
     //**************************************
     {
         name: "Kernel 2.6.35.9 – OC-UV-XTRA-BTN-AUD-RAM-NEON_FP",
-        summary: "1203 - Nexus - 21:30",
+        summary: "1207 - Nexus - 23:00",
         device: "passion",
-        url: "http://droidbasement.com/nexus/kernels/ram/2635/51/boot-cm_2635.9-oc-uv-xtra-btn-aud-ram-neon_fp-120310.zip"
+        url: "http://droidbasement.com/nexus/kernels/ram/2635/52/boot-cm_2635.9-oc-uv-xtra-btn-aud-ram-neon_fp-120710.zip"
     },
     //**************************************
     //**********Dream/Sapphire**************
@@ -28,31 +28,31 @@
     //32B - Dream/Sapphire - New Radio/SPL
     {
         name: "Kernel 2.6.35.9 - OC-UV-XTRA-RAM-FP - Radio 2708",
-        summary: "1203 - Dream/Sapphire (32B) - Radio 2708 - 21:30",
+        summary: "1207 - Dream/Sapphire (32B) - Radio 2708 - 23:00",
         devices:
         [
         "dream",
         "sapphire"
         ],
-        url: "http://droidbasement.com/g1/kernels/ram/2635/15/boot-cm_2635.9_ds_32b_radio2708-oc-uv-xtra-ram-fp-120310.zip"
+        url: "http://droidbasement.com/g1/kernels/ram/2635/16/boot-cm_2635.9_ds_32b_radio2708-oc-uv-xtra-ram-fp-120710.zip"
     },
     //32B - Dream/Sapphire
     {
         name: "Kernel 2.6.35.9 - OC-UV-XTRA-RAM-FP",
-        summary: "1203 - Dream/Sapphire (32B) - 21:30",
+        summary: "1207 - Dream/Sapphire (32B) - 23:00",
         devices:
         [
         "dream",
         "sapphire"
         ],
-        url: "http://droidbasement.com/g1/kernels/ram/2635/15/boot-cm_2635.9_ds_32b-oc-uv-xtra-ram-fp-120310.zip"
+        url: "http://droidbasement.com/g1/kernels/ram/2635/16/boot-cm_2635.9_ds_32b-oc-uv-xtra-ram-fp-120710.zip"
     },
     //32A - Magic
     {
         name: "Kernel 2.6.35.9 - OC-UV-XTRA-FP",
-        summary: "1203 - Dream/Sapphire (32A) - 21:30",
+        summary: "1207 - Dream/Sapphire (32A) - 23:00",
         device: "magic",
-        url: "http://droidbasement.com/g1/kernels/ram/2635/15/boot-cm_2635.9_ds_32a-oc-uv-xtra-ram-fp-120310.zip"
+        url: "http://droidbasement.com/g1/kernels/ram/2635/16/boot-cm_2635.9_ds_32a-oc-uv-xtra-ram-fp-120710.zip"
     }
     //END
     ]

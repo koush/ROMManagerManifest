2b77e135738780f176784cdcb44efb0b035fd18f
Koushik Dutta <koushd@gmail.com>
2010-12-12 18:11:05 -0800

    gapps

--- cyanogenmod.js
+++ cyanogenmod.js
@@ -149,7 +149,7 @@
         [
         {
             name: "Google Apps",
-            url: "http://android.d3xt3r01.tk/cyanogen/gapps/gapps-hdpi-20101114-signed.zip"
+            url: "http://phaseburn.net/~pb/gapps-hdpi-20101211-signed.zip"
         }
         ]
     },

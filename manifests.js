97a0f306c259164eab4444c82d0174567774539f
Koushik Dutta <koushd@gmail.com>
2010-12-14 20:39:29 -0800

    fix

--- manifests.js
+++ manifests.js
@@ -999,16 +999,6 @@
         id: "linuxmotion",
         summary: "Source built Roms and themes from LinuxMotion",
         manifest: "http://thelinuxmotion.org/ClockworkMod/manifest.js",
-        roms:
-        {
-            inc: true
-        }
-    },
-    {
-        developer: "Conical Roms",
-        id: "linuxmotion",
-        summary: "Source built Roms and themes from LinuxMotion",
-        manifest: "http://thelinuxmotion.org/ClockworkMod/manifest.js",
         free: true,
         roms:
         {

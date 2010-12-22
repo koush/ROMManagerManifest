0ab7a33da73d3612c9db25857dacd3c34bab9c70
Koushik Dutta <koushd@gmail.com>
2010-12-22 14:41:51 -0800

    fix bug

--- rommanager.js
+++ rommanager.js
@@ -1,24 +1,26 @@
-$.getJSON("http://rommanager.appspot.com/manifests/devices.js?callback=?", 
- function(data) {
-  var version = data.version;
-  var recovery_url = data.recovery_url;
-  var recovery_zip_url = data.recovery_zip_url;
+$.getJSON("http://rommanager.appspot.com/manifests/devices.js?callback=?",
+function(data) {
+    var version = data.version;
+    var recovery_url = data.recovery_url;
+    var recovery_zip_url = data.recovery_zip_url;
 
-  var contents = "";
-  $.each(data.devices, function(i, device) {
-   var v = version;
-   if (device.version != null && device.version > version) {
-    v = device.version;
-   }
-   var readonly_recovery = device.readonly_recovery;
-   var url = null;
-   if (readonly_recovery) {
-    url = sprintf(recovery_zip_url, v, device.key);
-   } else {
-    url = sprintf(recovery_url, v, device.key);
-   }
-   contents = contents + sprintf("<a href=\"%s\">%s</a><br/>", url, device.name);
-  });
-  $('.rommanagermanifest').html(contents);
- }
+    var contents = "";
+    $.each(data.devices,
+    function(i, device) {
+        var v = version;
+        if (device.version != null && device.version > version) {
+            v = device.version;
+        }
+        var readonly_recovery = device.readonly_recovery;
+        var url = null;
+        if (readonly_recovery) {
+            url = sprintf(recovery_zip_url, v, device.key);
+        } else {
+            url = sprintf(recovery_url, v, device.key);
+        }
+        contents = contents + sprintf("<a href=\"%s\">%s</a><br/>", url, device.name);
+    }
+    $('.rommanagermanifest').html(contents);
+    );
+}
 );

d887fdf058340e03ac11439d6ccc3f82865919bf
Koushik Dutta <koushd@gmail.com>
2010-12-24 21:59:47 -0800

    crepso

--- koush.js
+++ koush.js
@@ -11,14 +11,28 @@
     ],
     
     // Your list of ROMs
-    // =====HTC Incredible=====
     roms:
     [
     {
-        name: "Test Kernel",
-        summary: "Test/stock",
-        device: "inc",
-        file: "/htc/inc/inc-test.zip"
+        name: "CyanogenMod 7 Alpha",
+        summary: "Test/Alpha",
+        device: "crespo",
+        urls:
+        [
+        "http://koush.tandtgaming.com//samsung/crespo/update-cm-7.0.0-RC0-NS-signed.zip",
+        "http://koush.droid.co//samsung/crespo/update-cm-7.0.0-RC0-NS-signed.zip"
+        ],
+        addons:
+        [
+        {
+            name: "Google Apps",
+            urls:
+            [
+            "http://koush.tandtgaming.com//google/gapps-gb.zip",
+            "http://koush.droid.co//google/gapps-gb.zip"
+            ]
+        }
+        ]
     }
     ]
 }

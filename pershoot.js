61e8dc8603d082bdd7cd0494de8b83e6ef95ec5d
Prashant Somashekar <prashsomash@gmail.com>
2011-01-07 22:26:55 -0500

    0107 - harmony

--- pershoot.js
+++ pershoot.js
@@ -53,6 +53,13 @@
         summary: "1220 - Dream/Sapphire (32A) - 19:30",
         device: "magic",
         url: "http://droidbasement.com/g1/kernels/ram/2635/19/boot-cm_2635.10_ds_32a-oc-uv-xtra-ram-fp-122010.zip"
+    },
+    //Harmony - Gtab
+    {
+        name: "Kernel 2.6.32.27 - XTRA-VFPv3_FP",
+        summary: "0107 - Harmony (Gtab) - 22:30",
+        device: "harmony",
+        url: "http://droidbasement.com/gtab/kernels/2632/1/boot-cm_2632.27-xtra-vfpv3_fp-010711.zip"
     }
     //END
     ]

1bf494afa3675290720dbd8b1982dfcca39af7c6
Prashant Somashekar <prashsomash@gmail.com>
2011-01-09 07:16:00 -0500

    fix 0108 harmony dload links

--- pershoot.js
+++ pershoot.js
@@ -62,14 +62,14 @@
         name: "Kernel 2.6.32.27 - XTRA-VFPv3_FP",
         summary: "0108 - Harmony (Gtab) - 12:00",
         device: "harmony",
-        url: "http://droidbasement.com/gtab/kernels/2632/2/boot-cm_2632.27_gtab-xtra-vfpv3_fp-010810.zip"
+        url: "http://droidbasement.com/gtab/kernels/2632/2/boot-cm_2632.27_gtab-xtra-vfpv3_fp-010811.zip"
     },
     //Malata Zpad
     {
         name: "Kernel 2.6.32.27 - XTRA-VFPv3_FP",
         summary: "0108 - Harmony (Zpad) - 12:00",
         device: "harmony",
-        url: "http://droidbasement.com/gtab/kernels/2632/2/boot-cm_2632.27_zpad-xtra-vfpv3_fp-010810.zip"
+        url: "http://droidbasement.com/gtab/kernels/2632/2/boot-cm_2632.27_zpad-xtra-vfpv3_fp-010811.zip"
     }
     //END
     ]

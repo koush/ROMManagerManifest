4a6dcb02db6479c04916e01489cae50843490e4d
unknown <David@.(none)>
2011-02-05 15:05:08 -0500

    Added our team's repository

---
+++ ACS.js
@@ -0,0 +1,11 @@
+{
+    developer: "ACS",
+    id: "ACS",
+    summary: "ACS team roms",
+    manifest: "http://gh-pages.clockworkmod.com/ROMManagerManifest/cyanogenmod.js",
+    roms:
+    {
+        epic: true,
+        supersonic: true
+    }
+}

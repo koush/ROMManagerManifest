243d6e5be5ec42961c72131e91f90a05f510bc0c
Koushik Dutta <koushd@gmail.com>
2011-02-05 21:16:14 -0800

    acs

--- manifests.js
+++ manifests.js
@@ -1089,6 +1089,17 @@
             aloha: true,
             inc: true
         }
+    },
+    {
+        developer: "ACS",
+        id: "ACS",
+        summary: "ACS team roms",
+        manifest: "http://k0nane.info/rom/romlist.js",
+        roms:
+        {
+            epic: true,
+            supersonic: true
+        }
     }
     ]
 }

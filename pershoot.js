43b6e7f728b453094ea1c24ffe1d3c47c4b54095
Prashant Somashekar <prashsomash@gmail.com>
2011-02-08 00:01:58 -0500

    0207 - nexus/ds (froyo)

--- pershoot.js
+++ pershoot.js
@@ -22,43 +22,43 @@
     //**********Nexus**************
     //**************************************
     {
-        name: "Kernel 2.6.35.10 – OC-UV-XTRA-BTN-AUD-RAM-NEON_FP",
-        summary: "0126 - Nexus - 20:00",
+        name: "Kernel 2.6.35.11 – OC-UV-XTRA-BTN-AUD-RAM-NEON_FP",
+        summary: "0207 - Nexus (Froyo) - 23:00",
         device: "passion",
-        url: "http://droidbasement.com/nexus/kernels/ram/2635/58/boot-cm_2635.10-oc-uv-xtra-btn-aud-ram-neon_fp-012611.zip"
+        url: "http://droidbasement.com/nexus/kernels/ram/2635/59/boot-cm_2635.11-oc-uv-xtra-btn-aud-ram-neon_fp-020711.zip"
     },
     //**************************************
     //**********Dream/Sapphire**************
     //**************************************
     //
-    //32B - Dream/Sapphire - New Radio/SPL
+    //32B - Froyo - Dream/Sapphire - New Radio/SPL
     {
-        name: "Kernel 2.6.35.10 - OC-UV-XTRA-RAM-FP - Radio 2708",
-        summary: "0126 - Dream/Sapphire (32B) - Radio 2708 - 20:00",
+        name: "Kernel 2.6.35.11 - OC-UV-XTRA-RAM-FP - Radio 2708",
+        summary: "0207 - Dream/Sapphire (32B - Froyo) - Radio 2708 - 23:00",
         devices:
         [
         "dream",
         "sapphire"
         ],
-        url: "http://droidbasement.com/g1/kernels/ram/2635/21/boot-cm_2635.10_ds_32b_radio2708-oc-uv-xtra-ram-fp-012611.zip"
+        url: "http://droidbasement.com/g1/kernels/ram/2635/22/boot-cm_2635.11_ds_32b_radio2708-oc-uv-xtra-ram-fp-020711.zip"
     },
-    //32B - Dream/Sapphire
+    //32B - Froyo - Dream/Sapphire
     {
-        name: "Kernel 2.6.35.10 - OC-UV-XTRA-RAM-FP",
-        summary: "0126 - Dream/Sapphire (32B) - 20:00",
+        name: "Kernel 2.6.35.11 - OC-UV-XTRA-RAM-FP",
+        summary: "0207 - Dream/Sapphire (32B - Froyo) - 23:00",
         devices:
         [
         "dream",
         "sapphire"
         ],
-        url: "http://droidbasement.com/g1/kernels/ram/2635/21/boot-cm_2635.10_ds_32b-oc-uv-xtra-ram-fp-012611.zip"
+        url: "http://droidbasement.com/g1/kernels/ram/2635/22/boot-cm_2635.11_ds_32b-oc-uv-xtra-ram-fp-020711.zip"
     },
-    //32A - Magic
+    //32A - Froyo - Magic
     {
-        name: "Kernel 2.6.35.10 - OC-UV-XTRA-FP",
-        summary: "0126 - Dream/Sapphire (32A) - 20:00",
+        name: "Kernel 2.6.35.11 - OC-UV-XTRA-FP",
+        summary: "0207 - Dream/Sapphire (32A - Froyo) - 23:00",
         device: "magic",
-        url: "http://droidbasement.com/g1/kernels/ram/2635/21/boot-cm_2635.10_ds_32a-oc-uv-xtra-ram-fp-012611.zip"
+        url: "http://droidbasement.com/g1/kernels/ram/2635/22/boot-cm_2635.11_ds_32a-oc-uv-xtra-ram-fp-020711.zip"
     },
     //******Harmony**************
     //****************************
@@ -68,14 +68,14 @@
         name: "Kernel 2.6.32.28 - XTRA-VFPv3_FP",
         summary: "0201 - Harmony (Gtab/Zpad - Froyo) - 23:00",
         device: "harmony",
-        url: "http://droidbasement.com/gtab/kernels/2632/10/boot-cm_2632.28-xtra-vfpv3_fp-020111.zip"
+        url: "http://droidbasement.com/gtab/kernels/2632.11/boot-cm_2632.28-xtra-vfpv3_fp-020111.zip"
     },
     //ViewSonic Gtab and Malata Zpad (Gingerbread)
     {
         name: "Kernel 2.6.32.28 - XTRA-VFPv3_FP",
         summary: "0201 - Harmony (Gtab/Zpad - Gingerbread) - 23:00",
         device: "harmony",
-        url: "http://droidbasement.com/gtab/kernels/2632/10/boot-cm_2632.28_gb-xtra-vfpv3_fp-020111.zip"
+        url: "http://droidbasement.com/gtab/kernels/2632.11/boot-cm_2632.28_gb-xtra-vfpv3_fp-020111.zip"
     }
     //END
     ]

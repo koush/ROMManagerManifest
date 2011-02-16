de9ad7f880d58e83ce466de1715a0bbdff7d53a9
KalimochoAz <kalimochoaz@KalimochoAz-HPUbuntu.(none)>
2011-02-16 06:56:17 +0100

    Changed Click to propper naming

--- cyanogenmod.js
+++ cyanogenmod.js
@@ -1480,10 +1480,10 @@
     },
     {
         "device": "Click",
-        "name": "CyanogenMod 7.0.0-RC0-11.3",
+        "name": "CyanogenMod 7.0.0-RC0",
         "modversion": "CyanogenMod-7.0.0-RC0-Click",
         "incremental": "6101",
-        "url": "http://android.d3xt3r01.tk/cyanogen/click/update-cm-7.0.0-RC0-Click-signed-v11.3.zip",
+        "url": "http://android.d3xt3r01.tk/cyanogen/click/update-cm-7.0.0-RC0-Click-signed.zip",
         "product": "CyanogenMod",
         "summary": "Release Candidate 0",
         "addons": [

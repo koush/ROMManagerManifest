ce8d7959290d3fba57d6430f7b9634de8a994ee3
Shabbypenguin <shabbypenguin@gmail.com>
2011-02-22 13:40:32 -0800

    changed server

--- manifests.js
+++ manifests.js
@@ -1097,8 +1097,8 @@
     {
         developer: "ACS",
         id: "ACS",
-        summary: "ACS team roms",
-        manifest: "http://k0nane.info/rom/romlist.js",
+        summary: "Android Creative Syndicate",
+        manifest: "http://holylivingfuck.com/ACS/Romlist.js",
         roms:
         {
             epic: true,

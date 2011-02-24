9633da639c80105f2eaa8b05f504d38d24446287
Prashant Somashekar <prashsomash@gmail.com>
2011-02-24 09:38:34 -0500

    0223 - gtab/zpad

--- pershoot.js
+++ pershoot.js
@@ -65,17 +65,17 @@
     //
     //ViewSonic Gtab and Malata Zpad (Froyo)
     {
-        name: "Kernel 2.6.32.28 - XTRA-VFPv3_FP",
-        summary: "0218 - Harmony (Gtab/Zpad - Froyo) - 20:00",
+        name: "Kernel 2.6.32.29 - XTRA-VFPv3_FP",
+        summary: "0223 - Harmony (Gtab/Zpad - Froyo) - 23:00",
         device: "harmony",
-        url: "http://droidbasement.com/gtab/kernels/2632/13/boot-cm_2632.29-xtra-vfpv3_fp-021811.zip"
+        url: "http://droidbasement.com/gtab/kernels/2632/14/boot-cm_2632.29-xtra-vfpv3_fp-022311.zip"
     },
     //ViewSonic Gtab and Malata Zpad (Gingerbread)
     {
-        name: "Kernel 2.6.32.28 - XTRA-VFPv3_FP",
-        summary: "0218 - Harmony (Gtab/Zpad - Gingerbread) - 20:00",
+        name: "Kernel 2.6.32.29 - XTRA-VFPv3_FP",
+        summary: "0223 - Harmony (Gtab/Zpad - Gingerbread) - 23:00",
         device: "harmony",
-        url: "http://droidbasement.com/gtab/kernels/2632/13/boot-cm_2632.29_gb-xtra-vfpv3_fp-021811.zip"
+        url: "http://droidbasement.com/gtab/kernels/2632/14/boot-cm_2632.29_gb-xtra-vfpv3_fp-022311.zip"
     }
     //END
     ]

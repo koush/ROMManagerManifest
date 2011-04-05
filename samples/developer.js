381a454186f1436b38efe6e1e99bf981f61cde94
DrMacinyasha <drmacinyasha@gmail.com>
2011-04-05 01:32:18 -0700

    Update samples to latest style
    Just figured it'd be nice to have these up-to-date and matching the style currently used.

--- samples/developer.js
+++ samples/developer.js
@@ -1,19 +1,24 @@
-{
-    // This is what name you want your ROMs to show up under.
-    developer: "CyanogenMod",
-    // Your id. This should have no spaces or other funny characters.
-    // This would ideally be your Github id, so when you send me your manifest
-    // I can verify who you are. But it can be anything really.
-    id: "cyanogen",
-    // The display summary. Two lines max.
-    summary: "Mods and bacon for Dream, Sapphire, Droid, and Nexus One",
-    // The location of the developer's manifest.
-    manifest: "http://gh-pages.clockworkmod.com/ROMManagerManifest/cyanogenmod.js",
-    // The list of devices that this developer's ROMs support.
-    // If you don't support a device, just don't list it!
-    roms:
     {
-        sholes: true,
-        passion: true
-    }
-}
+        // This is what name you want your ROMs to show up under.
+        "developer": "CyanogenMod",
+        // Whether or not your ROM should be available to non-Premium
+        // users of ROM Manager. Usually best to leave it alone.
+        "free": "true",
+        // Your id. This should have no spaces or other funny characters.
+        // This would ideally be your Github id, so when you send me your manifest
+        // I can verify who you are. But it can be anything really.
+        "id": "cyanogenmod",
+        // The location of the developer's manifest.
+        "manifest": "http://gh-pages.clockworkmod.com/ROMManagerManifest/cyanogenmod.js",
+        // The list of devices that this developer's ROMs support.
+        // If you don't support a device, just don't list it!
+        // Make extra sure to watch those commas!
+        "roms": {
+            "dream": "true",
+            "passion": "true",
+            "sapphire": "true",
+            "sholes": "true"
+        },
+        // The display summary. Two lines max.
+        "summary": "Mods and bacon for your phone"
+    },
--- samples/simplemanifest.js
+++ samples/simplemanifest.js
@@ -1,23 +1,22 @@
 {
     // The version of the manifest, so ROM Manager knows how to load it
-    version: 1,
+    "version": "1",
     // The homepage for your ROM, if you have one.
-    homepage: "http://www.cyanogenmod.com/",
+    "homepage": "http://www.cyanogenmod.com/",
     // The donate link for yuor ROM, if you have one.
-    donate: "https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=3283920",
-    
+    "donate": "https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=3283920",
     // Your list of ROMs
-    roms:
+    "roms":
     [
     {
         // Display name of the rom
-        name: "CyanogenMod 5.0.5",
+        "name": "CyanogenMod 5.0.5",
         // Display summary of the rom
-        summary: "Stable",
+        "summary": "Stable",
         // Device that this rom runs on
-        device: "sholes",
+        "device": "sholes",
         // A screenshot to show off what the ROM looks like! (This is optional)
-        screenshot: "http://www.droidaftermarket.com/koush//motorola/sholes/screenshot2.png",
+        "screenshot": "http://www.droidaftermarket.com/koush//motorola/sholes/screenshot2.png",
         // This is the download URL of your ROM.
         // These URLs should have unique filenames per version!
         // Ie, don't use a zip with the same name
@@ -26,16 +25,14 @@
         // old cached zip on the SD card.
         // This must be a direct download,
         // and not a Mediafire type link.
-        url: "http://www.droidaftermarket.com/koush/motorola/sholes/cyanogen_sholes-ota-eng.koush_5.0.5.zip"
+        "url": "http://www.droidaftermarket.com/koush/motorola/sholes/cyanogen_sholes-ota-eng.koush_5.0.5.zip"
     },
-    
     // Antother, older ROM!
     {
-        name: "CyanogenMod 5.0.4",
-        summary: "Stable",
-        device: "sholes",
-        url: "http://www.droidaftermarket.com/koush/motorola/sholes/cyanogen_sholes-ota-eng.koush_5.0.4.zip"
+        "name": "CyanogenMod 5.0.4",
+        "summary": "Stable",
+        "device": "sholes",
+        "url": "http://www.droidaftermarket.com/koush/motorola/sholes/cyanogen_sholes-ota-eng.koush_5.0.4.zip"
     }
-
     ]
 }

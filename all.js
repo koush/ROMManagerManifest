15396a9be2ce4fe7a789cb6fae24d4cf2fcde7f8
DrMacinyasha <drmacinyasha@gmail.com>
2011-04-17 13:41:25 -0700

    Update su, clean up GApps

--- all.js
+++ all.js
@@ -3,16 +3,16 @@
     "roms":
     [
     {
-        "name": "Superuser 2.3.1 cd",
+        "name": "Superuser 2.3.6.1 cd",
         "summary": "Cupcake/Donut",
         "device": "all",
-        "url": "http://goo-inside.me/superuser/su-2.3.1-cd-signed.zip"
+        "url": "http://goo-inside.me/superuser/su-2.3.6.1-cd-signed.zip"
     },
     {
-        "name": "Superuser 2.3.1 ef",
-        "summary": "Eclair/Froyo",
+        "name": "Superuser 2.3.6.1 ef",
+        "summary": "Eclair/Froyo/Gingerbread",
         "device": "all",
-        "url": "http://goo-inside.me/superuser/su-2.3.1-ef-signed.zip"
+        "url": "http://goo-inside.me/superuser/su-2.3.6.1-ef-signed.zip"
     }    
     ]
 }

7e42640419bcc6d784001317880e30f3bf46c5fe
roalex <giant@kappa.ro>
2011-04-21 19:25:09 +0800

    a space :)

--- manifests.js
+++ manifests.js
@@ -1071,7 +1071,7 @@
             "sapphire": "true" 
         },
           "summary": "A AOSP(using the latest CM/EzGB sources)rom for D/S (supporting Ebi0,2708,Ebi1 devices")
-     }
+    }
     ],
     "minversion": "2000"
 }

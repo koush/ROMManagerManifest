89583cbda6a96c14d2baf598d5925f7a3bff36ad
Hamsterkacke <nophtu@gmail.com>
2011-04-25 09:12:42 -0700

    Edited and removed errors from my first edit attempt. Should be okay now

--- manifests.js
+++ manifests.js
@@ -1081,9 +1081,9 @@
     "manifest": "https://github.com/Hamsterkacke/Hamsterbread-Rom-Manager-Stuff/blob/master/manifest.js",
     "roms": {
         "galaxys": "true" 
+         },
+         "summary": "Some Hamster Love for your phone!" 
     },
-    "summary": "Some Hamster Love for your phone!" 
-    }
     {
         "developer": "Google Apps",
         "free": "true",

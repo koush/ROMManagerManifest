19cc5075137fe863e219ec4d79d57b5702fc7a20
Koushik Dutta <koushd@gmail.com>
2011-04-24 17:58:25 -0700

    another build that should fix ext2 system mounting

--- devices.js
+++ devices.js
@@ -809,18 +809,22 @@
     },
     {
       "name": "T-Mobile G2X",
+      "reboot_recovery": "echo 'boot-recovery' | dd of=/dev/block/mmcblk0p3 seek=6144 bs=1 ; reboot ; ",
       "version": "3.0.2.7",
       "init": "init_recovery.rc",
       "readonly_recovery": true,
-      "reboot_recovery": "echo 'boot-recovery' | dd of=/dev/block/mmcblk0p3 seek=6144 bs=1 ; reboot ; ",
       "key": "p999"
     },
     {
       "name": "LG Optimus 2X",
-      "version": "3.0.2.7",
-      "init": "init_recovery.rc",
       "flash_recovery": "dd if=%s of=/dev/block/mmcblk0p7 bs=4096 ; ",
       "reboot_recovery": "echo 'boot-recovery' | dd of=/dev/block/mmcblk0p3 seek=6144 bs=1 ; reboot ; ",
+      "version": "3.0.2.8",
+      "init": "init_recovery.rc",
+      "legacy_versions": [
+        "3.0.2.7",
+        "3.0.2.7"
+      ],
       "key": "p990"
     }
   ],

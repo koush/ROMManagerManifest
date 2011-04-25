c9b2d7c7635ffc591df53fc5853de3f54d0963e3
Koushik Dutta <koushd@gmail.com>
2011-04-24 18:18:23 -0700

    Revert "Revert "test""
    This reverts commit 2879e47941d5cb406baafe12511e199b3207b2e2.

--- cyanogenmod.js
+++ cyanogenmod.js
@@ -561,6 +561,13 @@
     },
     {
         "device": "crespo",
+        "name": "test",
+        "modversion": "crepsotest",
+        "url": "http://download.cyanogenmod.com/get/update-cm-7.0.0-NS-signed.zip",
+        "summary": "Stable Release"
+    },
+    {
+        "device": "crespo",
         "name": "CyanogenMod 7.0.0",
         "modversion": "CyanogenMod-7.0.0-NS",
         "incremental": "7005",

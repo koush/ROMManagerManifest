ef6da6136f8066ddd8c6d9d780754879ebe8c585
Koushik Dutta <koushd@gmail.com>
2011-05-13 11:29:42 -0700

    point to pershoot

--- manifests.js
+++ manifests.js
@@ -274,7 +274,7 @@
       "developer": "Pershoot",
       "id": "pershoot",
       "free": "true",
-      "manifest": "http://gh-pages.clockworkmod.com/ROMManagerManifest/pershoot.js",
+      "manifest": "http://pershoot.github.com/ROMManagerManifest/pershoot.js",
       "roms": {
         "passion": "true",
         "magic": "true",

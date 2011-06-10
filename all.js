ba8dc0991499aed1db382512e10a401899915a88
DrMacinyasha <drmacinyasha@gmail.com>
2011-06-10 15:01:31 -0700

    Superuser 2.3.6.2 from ChainsDD

--- all.js
+++ all.js
@@ -3,16 +3,16 @@
     "roms":
     [
     {
-        "name": "Superuser 2.3.6.1 cd",
-        "summary": "Cupcake/Donut",
+        "name": "Superuser 2.3.6.2 cd",
+        "summary": "Cupcake and Donut",
         "device": "all",
-        "url": "http://goo-inside.me/superuser/su-2.3.6.1-cd-signed.zip"
+        "url": "http://goo-inside.me/superuser/su-2.3.6.2-cd-signed.zip"
     },
     {
-        "name": "Superuser 2.3.6.1 ef",
-        "summary": "Eclair/Froyo/Gingerbread",
+        "name": "Superuser 2.3.6.2 efgh",
+        "summary": "Eclair, Froyo, Gingerbread, and Honeycomb",
         "device": "all",
-        "url": "http://goo-inside.me/superuser/su-2.3.6.1-ef-signed.zip"
+        "url": "http://goo-inside.me/superuser/su-2.3.6.2-efgh-signed.zip"
     }    
     ]
 }

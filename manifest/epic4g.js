e6ae4ecba63118e493dec0665c33d19ec486558e
Koushik Dutta <koushd@gmail.com>
2011-06-16 11:22:07 -0700

    remove acs

--- manifest/epic4g.js
+++ manifest/epic4g.js
@@ -30,14 +30,6 @@
       "free": "true",
       "manifest": "http://drmacinyasha.github.com/proprietary_vendor_google/manifest.js",
       "summary": "Add-on for AOSP ROMs, such as CM"
-    },
-    {
-      "developer": "ACSyndicate",
-      "free": true,
-      "icon": "http://romshare.deployfu.com/downloads/272/f1369e30d685f847faa5fd8129e9c330.png",
-      "id": "ACS",
-      "manifest": "http://romshare.deployfu.com/developer/ACS/manifest",
-      "summary": "Here you will find all of ACSyndicates Latest and older releases."
     }
   ],
   "minversion": "2000"

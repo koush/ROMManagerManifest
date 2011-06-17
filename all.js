0ab865bbea07ea4745cf2f730f7a15ac24285a69
DrMacinyasha <drmacinyasha@gmail.com>
2011-06-17 15:38:36 -0700

    Add ChainsDD's site as a mirror for Superuser

--- all.js
+++ all.js
@@ -1,18 +1,23 @@
 {
     "version": "1",
-    "roms":
-    [
-    {
-        "name": "Superuser 2.3.6.3 cd",
-        "summary": "Cupcake and Donut",
-        "device": "all",
-        "url": "http://goo-inside.me/superuser/su-2.3.6.3-cd-signed.zip"
-    },
-    {
-        "name": "Superuser 2.3.6.3 efgh",
-        "summary": "Eclair, Froyo, Gingerbread, and Honeycomb",
-        "device": "all",
-        "url": "http://goo-inside.me/superuser/su-2.3.6.3-efgh-signed.zip"
-    }    
+    "roms": [
+        {
+            "name": "Superuser 2.3.6.3 cd",
+            "summary": "Cupcake and Donut",
+            "device": "all",
+            "urls": [
+                "http://goo-inside.me/superuser/su-2.3.6.3-cd-signed.zip",
+                "http://downloads.androidsu.com/superuser/su-2.3.6.3-cd-signed.zip"
+            ]
+        },
+        {
+            "name": "Superuser 2.3.6.3 efgh",
+            "summary": "Eclair, Froyo, Gingerbread, and Honeycomb",
+            "device": "all",
+            "urls": [
+                "http://goo-inside.me/superuser/su-2.3.6.3-efgh-signed.zip",
+                "http://downloads.androidsu.com/superuser/su-2.3.6.3-efgh-signed.zip"
+            ]
+        }
     ]
 }

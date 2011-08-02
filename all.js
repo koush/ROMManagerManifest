02f85a3f06352a968f027cdca4fb00874e069d18
Denham Crafton <drmacinyasha@gmail.com>
2011-08-02 19:33:57 +0000

    Superuser 3.0 Beta 3
    Per ChainsDD's request

--- all.js
+++ all.js
@@ -18,6 +18,24 @@
                 "http://goo-inside.me/superuser/su-2.3.6.3-efgh-signed.zip",
                 "http://downloads.androidsu.com/superuser/su-2.3.6.3-efgh-signed.zip"
             ]
+        },
+        {
+            "name": "Superuser 3.0 Beta 3 cd",
+            "summary": "Cupcake and Donut",
+            "device": "all",
+            "urls": [
+                "http://goo-inside.me/superuser/su-3.0-beta3-cd-signed.zip",
+                "http://downloads.androidsu.com/superuser/su-3.0-beta3-cd-signed.zip"
+            ]
+        },
+        {
+            "name": "Superuser 3.0 Beta 3 efgh",
+            "summary": "Eclair, Froyo, Gingerbread, and Honeycomb",
+            "device": "all",
+            "urls": [
+                "http://goo-inside.me/superuser/su-3.0-beta3-efgh-signed.zip",
+                "http://downloads.androidsu.com/superuser/su-3.0-beta3-efgh-signed.zip"
+            ]
         }
     ]
 }

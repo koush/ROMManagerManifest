0f911e444aca6091bb7b690f11d0ed8db8595175
Koushik Dutta <koushd@gmail.com>
2011-08-31 23:58:27 -0700

    d2 rebuild

---
+++ manifest/p970.js
@@ -0,0 +1,21 @@
+{
+  "manifests": [
+    {
+      "developer": "Extras",
+      "free": "true",
+      "id": "extras",
+      "icon": "http://koush.kanged.net/cm/test/extras.png",
+      "manifest": "http://gh-pages.clockworkmod.com/ROMManagerManifest/all.js",
+      "summary": "Cool stuffs."
+    },
+    {
+      "developer": "Google Apps",
+      "free": "true",
+      "id": "google",
+      "icon": "http://drmacinyasha.github.com/proprietary_vendor_google/icon.png",
+      "manifest": "http://drmacinyasha.github.com/proprietary_vendor_google/manifest.js",
+      "summary": "Add-on for AOSP ROMs, such as CM"
+    }
+  ],
+  "minversion": "4403"
+}

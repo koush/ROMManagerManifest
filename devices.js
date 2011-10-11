007491bbe01e2a5ef4da46b48959a891746a49df
Koushik Dutta <koushd@gmail.com>
2011-10-11 16:47:11 -0700

    fix up mtd, how did this ever work???

--- devices.js
+++ devices.js
@@ -772,21 +772,18 @@
     {
       "name": "Captivate (MTD)",
       "init": "init.smdkc110.rc",
-      "readonly_recovery": true,
       "officially_supported": false,
       "key": "captivatemtd"
     },
     {
       "name": "Vibrant (MTD)",
       "init": "init.smdkc110.rc",
-      "readonly_recovery": true,
       "officially_supported": false,
       "key": "vibrantmtd"
     },
     {
       "name": "GalaxyS i9000 (MTD)",
       "init": "lpm.rc",
-      "readonly_recovery": true,
       "officially_supported": false,
       "key": "galaxysmtd"
     },

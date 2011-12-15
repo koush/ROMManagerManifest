276d7fcdd825e9d93770182aa974d069ad53b914
Koushik Dutta <koushd@gmail.com>
2011-12-15 06:39:19 -0800

    rom manager.

--- clockworkmod.js
+++ clockworkmod.js
@@ -6,6 +6,12 @@
         "icon": "https://github.com/ClockworkMod/artwork/raw/master/desksms/desksms.png",
         "summary": "Text from your computer or tablet with your existing phone number!",
         "packagename": "com.koushikdutta.desktopsms"
+    },
+    {
+        "name": "ROM Manager",
+        "icon": "https://github.com/ClockworkMod/artwork/raw/master/rommanager/clockwork128.png",
+        "summary": "Text from your computer or tablet with your existing phone number!",
+        "packagename": "com.koushikdutta.rommanager"
     }
     ],
     "roms": [

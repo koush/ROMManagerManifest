aeeab8971f515e3260e5d0c85a72bcbf142fc602
Koushik Dutta <koushd@gmail.com>
2011-12-14 19:55:48 -0800

    desksms

---
+++ clockworkmod.js
@@ -0,0 +1,14 @@
+{
+    "version": "1",
+    "homepage": "http://www.cyanogenmod.com/",
+    "donate": "https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=3283920",
+    "marketapps": [
+    {
+        "name": "DeskSMS",
+        "summary": "Text from your computer or tablet with your existing phone number!",
+        "packagename": "com.koushikdutta.desktopsms"
+    }
+    ]
+    "roms": [
+    ]
+}

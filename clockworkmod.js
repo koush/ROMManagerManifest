994deb3ee9e3cb1987dbdee72ab83b0d1629fcda
Koushik Dutta <koushd@gmail.com>
2011-12-14 19:58:57 -0800

    fix

--- clockworkmod.js
+++ clockworkmod.js
@@ -3,11 +3,11 @@
     "marketapps": [
     {
         "name": "DeskSMS",
-        "icon": "https://github.com/ClockworkMod/artwork/raw/master/desksms/desksms.png"
+        "icon": "https://github.com/ClockworkMod/artwork/raw/master/desksms/desksms.png",
         "summary": "Text from your computer or tablet with your existing phone number!",
         "packagename": "com.koushikdutta.desktopsms"
     }
-    ]
+    ],
     "roms": [
     ]
 }

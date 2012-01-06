50fe0ba9dec3af643d056deba16dfdbae5b50567
Koushik Dutta <koushd@gmail.com>
2012-01-05 22:42:49 -0800

    move this out

--- devices.js
+++ devices.js
@@ -1,31 +1,6 @@
 {
   "pontiflex_startup_register": 100,
   "use_in_app": false,
-  "driver_url": "http://download.clockworkmod.com/drivers/%s",
-  "drivers": {
-    "samsung": {
-      "soju": "google-adb-drivers.zip",
-      "sojua": "google-adb-drivers.zip",
-      "maguro": "Verizon_Wireless_I515_Galaxy_Nexus_USB_Driver_v1.4.6.0.exe",
-      "toro": "Verizon_Wireless_I515_Galaxy_Nexus_USB_Driver_v1.4.6.0.exe",
-      "universal": "SAMSUNG_USB_Driver_for_Mobile_Phones.exe"
-    },
-    "htc": {
-      "universal": "HTCDriver3.0.0.007.exe",
-      "passion": "google-adb-drivers.zip",
-      "dream": "google-adb-drivers.zip",
-      "sapphire": "google-adb-drivers.zip"
-    },
-    "motorola": {
-      "universal": "MotoHelper_2.1.32_Driver_5.4.0.exe"
-    },
-    "lge": {
-      "universal": "LGUnitedMobileDriver_S498MA22_WHQL_ML_Ver_2.2.exe"
-    },
-    "zte": {
-      "universal": "ZTE_Android_Driver.exe"
-    }
-  },
   "devices": [
     {
       "name": "Motorola Droid",

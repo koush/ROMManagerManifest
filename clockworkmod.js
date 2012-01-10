aa7db591d3d55b9020afadcbbfe8bef7c7c23558
Koushik Dutta <koushd@gmail.com>
2012-01-09 19:52:06 -0800

    tether

--- clockworkmod.js
+++ clockworkmod.js
@@ -2,6 +2,12 @@
     "version": "1",
     "marketapps": [
     {
+        "name": "Tether",
+        "icon": "https://github.com/ClockworkMod/artwork/raw/master/tether/tether_icon_usb_dark_512.png",
+        "summary": "Tether your laptop using USB to get internet access through your phone!",
+        "packagename": "com.koushikdutta.tether"
+    },
+    {
         "name": "DeskSMS",
         "icon": "https://github.com/ClockworkMod/artwork/raw/master/desksms/desksms.png",
         "summary": "Text from your computer or tablet with your existing phone number!",

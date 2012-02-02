c58636dfece152ef6fd15b1c88d14d6be9d11f21
Koushik Dutta <koushd@gmail.com>
2012-02-02 01:42:26 -0800

    Revert "Revert "glacier and vision""
    This reverts commit 59c491e70bec696115b5c29431b13ecb758c1eaa.

--- devices.js
+++ devices.js
@@ -457,6 +457,7 @@
     {
       "name": "HTC G2",
       "flash_recovery": "dd if=/dev/zero of=/dev/block/mmcblk0p21 bs=4096 ; dd if=%s of=/dev/block/mmcblk0p21 bs=4096 ; ",
+      "touch_version": "5.8.0.9",
       "version": "5.0.2.7",
       "init": "init.vision.rc",
       "legacy_versions": [
@@ -479,6 +480,7 @@
       "name": "HTC MyTouch 4G",
       "flash_recovery": "dd if=%s of=/dev/block/mmcblk0p21 ; ",
       "version": "5.0.2.0",
+      "touch_version": "5.8.0.9",
       "init": "init.glacier.rc",
       "legacy_versions": [
         "3.0.2.4",

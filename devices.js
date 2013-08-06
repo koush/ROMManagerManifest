134e3b9f44fac35782dc3ad1ebb9c99fba0a2453
Dewayne Jones <playfulgod@gmail.com>
2013-08-06 18:20:18 -0400

    Update devices.js
    merge changes for Huawei Ascend Q by Shabbypenguin

--- devices.js
+++ devices.js
@@ -3299,15 +3299,15 @@
       "key": "jfltecsp",
       "touch_version": "6.0.3.5"
     },
-    {
-      "version": "6.0.3.5",
-      "lunch": "cm_m660-userdebug",
-      "name": "m660",
-      "init": "init.m660.rc",
+     "flash_recovery": "dd if=%s of=/dev/block/mmcblk0p16 ; ",
+      "name": "Huawei Ascend Q",
       "legacy_versions": [
 
       ],
-      "key": "m660"
+      "lunch": "cm_m660-userdebug",
+      "key": "m660",
+      "version": "6.0.3.5",
+      "init": "init.highmem.rc"
     }
   ]
 }

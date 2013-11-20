674ee37cdded697f663332fd46af706b9579c311
hudson@koushik-lion <jenkins@cyanogenmod.com>
2013-11-20 05:10:10 -0800

    builder: hammerhead updated on behalf of koush@clockworkmod.com 45852: http://jenkins.cyanogenmod.com/job/recovery/45852

--- devices.js
+++ devices.js
@@ -3626,7 +3626,8 @@
 
       ],
       "version": "6.0.4.4",
-      "lunch": "cm_hammerhead-userdebug"
+      "lunch": "cm_hammerhead-userdebug",
+      "touch_version": "6.0.4.4"
     }
   ],
   "manifest_signature": "O8+Kc2mdQz0e/AjCQn6Rw6fK5Kr5LZItJ474Ua3gF2o4IgdCQZXIbwCHIo6xeqY3pVEGtjYmmyO1PkRYcGqq2rB2wQM8SoKFl/70vG0ihEZ5TDeruKUgxVVSNM3e09kBvCxYK89GWqVWkNHtvTu1T1F9w6FilzCsNFSAXHD0Bos=",

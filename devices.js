4b9db1720fda55f28ae8b53b2fe855b69151d273
hudson@koushik-lion <jenkins@cyanogenmod.com>
2014-01-01 11:47:43 -0800

    builder: expressatt updated on behalf of njgreb@gmail.com 50133: http://jenkins.cyanogenmod.com/job/recovery/50133

--- devices.js
+++ devices.js
@@ -3644,7 +3644,9 @@
       "legacy_versions": [
 
       ],
-      "touch_version": "6.0.4.5"
+      "touch_version": "6.0.4.5",
+      "version": "6.0.4.5",
+      "lunch": "cm_expressatt-userdebug"
     }
   ],
   "manifest_signature": "O8+Kc2mdQz0e/AjCQn6Rw6fK5Kr5LZItJ474Ua3gF2o4IgdCQZXIbwCHIo6xeqY3pVEGtjYmmyO1PkRYcGqq2rB2wQM8SoKFl/70vG0ihEZ5TDeruKUgxVVSNM3e09kBvCxYK89GWqVWkNHtvTu1T1F9w6FilzCsNFSAXHD0Bos=",

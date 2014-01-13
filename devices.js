549a8e395d97f3099457ab6978527f0f7aebcc9a
hudson@koushik-lion <jenkins@cyanogenmod.com>
2014-01-12 19:51:34 -0800

    builder: hltexx updated on behalf of koush@clockworkmod.com 51907: http://jenkins.cyanogenmod.com/job/recovery/51907

--- devices.js
+++ devices.js
@@ -3683,6 +3683,16 @@
       ],
       "version": "6.0.4.6",
       "lunch": "cm_hltevzw-userdebug"
+    },
+    {
+      "key": "hltexx",
+      "init": "init.hltexx.rc",
+      "name": "hltexx",
+      "legacy_versions": [
+
+      ],
+      "version": "6.0.4.6",
+      "lunch": "cm_hltexx-userdebug"
     }
   ],
   "manifest_signature": "O8+Kc2mdQz0e/AjCQn6Rw6fK5Kr5LZItJ474Ua3gF2o4IgdCQZXIbwCHIo6xeqY3pVEGtjYmmyO1PkRYcGqq2rB2wQM8SoKFl/70vG0ihEZ5TDeruKUgxVVSNM3e09kBvCxYK89GWqVWkNHtvTu1T1F9w6FilzCsNFSAXHD0Bos=",

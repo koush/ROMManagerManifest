393b6fc69e21e0bfc108f090d24c1ed0cdb5bb83
hudson@koushik-lion <jenkins@cyanogenmod.com>
2014-01-12 19:19:47 -0800

    builder: hltetmo updated on behalf of koush@clockworkmod.com 51904: http://jenkins.cyanogenmod.com/job/recovery/51904

--- devices.js
+++ devices.js
@@ -3662,6 +3662,16 @@
 
       ],
       "touch_version": "6.0.4.6"
+    },
+    {
+      "key": "hltetmo",
+      "init": "init.hltetmo.rc",
+      "name": "hltetmo",
+      "legacy_versions": [
+
+      ],
+      "version": "6.0.4.6",
+      "lunch": "cm_hltetmo-userdebug"
     }
   ],
   "manifest_signature": "O8+Kc2mdQz0e/AjCQn6Rw6fK5Kr5LZItJ474Ua3gF2o4IgdCQZXIbwCHIo6xeqY3pVEGtjYmmyO1PkRYcGqq2rB2wQM8SoKFl/70vG0ihEZ5TDeruKUgxVVSNM3e09kBvCxYK89GWqVWkNHtvTu1T1F9w6FilzCsNFSAXHD0Bos=",

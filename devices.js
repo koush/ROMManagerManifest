74577eaa20ba56f46226b5c3db99b1bb675b0fb0
hudson@koushik-lion <jenkins@cyanogenmod.com>
2014-01-14 11:55:18 -0800

    builder: m7 updated on behalf of koush@clockworkmod.com 52201: http://jenkins.cyanogenmod.com/job/recovery/52201

--- devices.js
+++ devices.js
@@ -2862,9 +2862,10 @@
       "key": "m7",
       "legacy_versions": [
         "6.0.2.8",
-        "6.0.3.0"
+        "6.0.3.0",
+        "6.0.3.1"
       ],
-      "version": "6.0.3.1",
+      "version": "6.0.4.6",
       "flash_recovery": "dd if=%s of=/dev/block/mmcblk0p34 bs=4096 ; "
     },
     {

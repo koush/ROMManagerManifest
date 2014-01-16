4c05b36de99fc5af3c8c7b570d4b1586bab84ad8
hudson@koushik-lion <jenkins@cyanogenmod.com>
2014-01-16 14:56:41 -0800

    builder: find5 updated on behalf of njgreb@gmail.com 52497: http://jenkins.cyanogenmod.com/job/recovery/52497

--- devices.js
+++ devices.js
@@ -3182,7 +3182,7 @@
     {
       "name": "Oppo Find5",
       "lunch": "cm_find5-userdebug",
-      "touch_version": "6.0.4.5",
+      "touch_version": "6.0.4.6",
       "init": "init.find5.rc",
       "key": "find5",
       "legacy_versions": [

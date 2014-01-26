32c9f25ca0c00b949ef356ed2e3600bbec343989
Koushik Dutta <koushd@gmail.com>
2014-01-26 15:18:13 -0800

    format

--- gapps.js
+++ gapps.js
@@ -1,65 +1,65 @@
 {
-    "version": "1",
     "roms": [
-	    {
-	        "name": "Google Apps 4.4",
-	        "summary": "Recommended for CM 11.0.x\nAugust 13, 2013 for Android 4.4.X",
-	        "product": "kk",
-	        "modversion": "GApps-20131119-KK",
-	        "incremental": "20131119",
-	        "device": "all",
-	        "url": "http://download.clockworkmod.com/gapps/gapps-kk-20140105-signed.zip"
-	    },
-	    {
-	        "name": "Google Apps 4.3",
-	        "summary": "Recommended for CM 10.2.x\nAugust 13, 2013 for Android 4.3.X",
-	        "product": "jb",
-	        "modversion": "GApps-20130813-JB",
-	        "incremental": "20130813",
-	        "device": "all",
-	        "url": "http://download.clockworkmod.com/gapps/gapps-jb-20130813-signed.zip"
-	    },
         {
-            "name": "Google Apps 4.2",
-            "summary": "Recommended for CM 10.1.x\nJuly 26, 2012 for Android 4.2.X",
-            "product": "jb",
-            "modversion": "GApps-20121212-JB",
-            "incremental": "20121212",
             "device": "all",
+            "incremental": "20131119",
+            "modversion": "GApps-20131119-KK",
+            "name": "Google Apps 4.4",
+            "product": "kk",
+            "summary": "Recommended for CM 11.0.x\nAugust 13, 2013 for Android 4.4.X",
+            "url": "http://download.clockworkmod.com/gapps/gapps-kk-20140105-signed.zip"
+        },
+        {
+            "device": "all",
+            "incremental": "20130813",
+            "modversion": "GApps-20130813-JB",
+            "name": "Google Apps 4.3",
+            "product": "jb",
+            "summary": "Recommended for CM 10.2.x\nAugust 13, 2013 for Android 4.3.X",
+            "url": "http://download.clockworkmod.com/gapps/gapps-jb-20130813-signed.zip"
+        },
+        {
+            "device": "all",
+            "incremental": "20121212",
+            "modversion": "GApps-20121212-JB",
+            "name": "Google Apps 4.2",
+            "product": "jb",
+            "summary": "Recommended for CM 10.1.x\nJuly 26, 2012 for Android 4.2.X",
             "url": "http://download.clockworkmod.com/gapps/gapps-jb-20121212-signed.zip"
         },
         {
-            "name": "Google Apps 4.1",
-            "summary": "Recommended for CM 10.0.x\nJuly 26, 2012 for Android 4.1.1",
-            "product": "jb",
-            "modversion": "GApps-20121011-JB",
-            "incremental": "20121011",
             "device": "all",
+            "incremental": "20121011",
+            "modversion": "GApps-20121011-JB",
+            "name": "Google Apps 4.1",
+            "product": "jb",
+            "summary": "Recommended for CM 10.0.x\nJuly 26, 2012 for Android 4.1.1",
             "url": "http://download.clockworkmod.com/gapps/gapps-jb-20121011-signed.zip"
         },
         {
-            "name": "Google Apps 4.0",
-            "summary": "Recommended for CM 9.0.x\nApril 29, 2012 for Android 4.0.4",
-            "product": "ics",
-            "modversion": "GApps-20120429-ICS",
-            "incremental": "20120422",
             "device": "all",
+            "incremental": "20120422",
+            "modversion": "GApps-20120429-ICS",
+            "name": "Google Apps 4.0",
+            "product": "ics",
+            "summary": "Recommended for CM 9.0.x\nApril 29, 2012 for Android 4.0.4",
             "url": "http://download.clockworkmod.com/gapps/gapps-ics-20120429-signed.zip"
         },
         {
-            "name": "Google Apps 2.3",
-            "summary": "Recommended for CM 7.1.x\nAugust 28, 2011 for Android 2.3.5",
-            "product": "gingerbread",
-            "modversion": "GApps-20110828-Universal",
-            "incremental": "20110828",
-            "device": "all",
-            "url": "http://download.clockworkmod.com/gapps/gapps-gb-20110828-signed.zip",
             "addons": [
                 {
                     "name": "GTalk w/ Video Chat (Experimental!)",
                     "url": "http://download.clockworkmod.com/gapps/gapps-gb-20110828-newtalk-signed.zip"
                 }
-            ]
+            ],
+            "device": "all",
+            "incremental": "20110828",
+            "modversion": "GApps-20110828-Universal",
+            "name": "Google Apps 2.3",
+            "product": "gingerbread",
+            "summary": "Recommended for CM 7.1.x\nAugust 28, 2011 for Android 2.3.5",
+            "url": "http://download.clockworkmod.com/gapps/gapps-gb-20110828-signed.zip"
         }
-    ]
+    ],
+    "version": "1"
 }

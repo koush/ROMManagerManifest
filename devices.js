f9f5bd7a71e7a0acf96eb4473a95fbb47e7d69bb
hudson@koushik-lion <jenkins@cyanogenmod.com>
2014-02-10 19:30:01 -0800

    builder: endeavoru updated on behalf of njgreb@gmail.com 55509: http://jenkins.cyanogenmod.com/job/recovery/55509

--- devices.js
+++ devices.js
@@ -1861,7 +1861,7 @@
     {
       "name": "HTC One X (GSM)",
       "lunch": "cm_endeavoru-userdebug",
-      "touch_version": "5.8.4.0",
+      "touch_version": "6.0.4.7",
       "init": "ueventd.endeavoru.rc",
       "key": "endeavoru",
       "legacy_versions": [

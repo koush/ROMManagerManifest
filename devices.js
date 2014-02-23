0984073e1e5a0970e5adb099f365d0151b83ef55
hudson@koushik-lion <jenkins@cyanogenmod.com>
2014-02-23 12:05:45 -0800

    builder: jflte updated on behalf of playfulgod@gmail.com 56977: http://jenkins.cyanogenmod.com/job/recovery/56977

--- devices.js
+++ devices.js
@@ -3760,7 +3760,9 @@
       "legacy_versions": [
 
       ],
-      "touch_version": "6.0.4.7"
+      "touch_version": "6.0.4.7",
+      "version": "6.0.4.7",
+      "lunch": "cm_jflte-userdebug"
     }
   ],
   "manifest_signature": "O8+Kc2mdQz0e/AjCQn6Rw6fK5Kr5LZItJ474Ua3gF2o4IgdCQZXIbwCHIo6xeqY3pVEGtjYmmyO1PkRYcGqq2rB2wQM8SoKFl/70vG0ihEZ5TDeruKUgxVVSNM3e09kBvCxYK89GWqVWkNHtvTu1T1F9w6FilzCsNFSAXHD0Bos=",

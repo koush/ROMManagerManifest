74b628664f2f5acb8d50189bac10eeea4e64a449
hudson@koushik-lion <jenkins@cyanogenmod.com>
2014-04-28 12:47:51 -0700

    builder: evita updated on behalf of brint@cyngn.com 61993: http://jenkins.cyanogenmod.com/job/recovery/61993

--- devices.js
+++ devices.js
@@ -2973,9 +2973,10 @@
       "key": "evita",
       "legacy_versions": [
         "6.0.2.8",
-        "6.0.3.1"
+        "6.0.3.1",
+        "6.0.4.6"
       ],
-      "version": "6.0.4.6",
+      "version": "6.0.4.8",
       "flash_recovery": "dd if=%s of=/dev/block/mmcblk0p22 bs=4096 ; "
     },
     {

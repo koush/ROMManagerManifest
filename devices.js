aa7841ae48e7eb031321eec58caf06b4c656101c
hudson@koushik-lion <jenkins@cyanogenmod.com>
2014-05-06 15:00:23 -0700

    builder: enrc2b updated on behalf of njgreb@gmail.com 62864: http://jenkins.cyanogenmod.com/job/recovery/62864

--- devices.js
+++ devices.js
@@ -3805,6 +3805,16 @@
       "version": "6.0.4.8",
       "lunch": "cm_m4-userdebug",
       "touch_version": "6.0.4.8"
+    },
+    {
+      "key": "enrc2b",
+      "init": "init.enrc2b.rc",
+      "name": "enrc2b",
+      "legacy_versions": [
+
+      ],
+      "version": "6.0.4.8",
+      "lunch": "cm_enrc2b-userdebug"
     }
   ],
   "manifest_signature": "O8+Kc2mdQz0e/AjCQn6Rw6fK5Kr5LZItJ474Ua3gF2o4IgdCQZXIbwCHIo6xeqY3pVEGtjYmmyO1PkRYcGqq2rB2wQM8SoKFl/70vG0ihEZ5TDeruKUgxVVSNM3e09kBvCxYK89GWqVWkNHtvTu1T1F9w6FilzCsNFSAXHD0Bos=",
